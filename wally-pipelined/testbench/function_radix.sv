7902c3fdb69e62b78cccf03e37af0a55d4533055
Ross Thompson <stephen.thompson.37@us.af.mil>
2021-03-04 17:31:12 -0600

    updated the function radix to look at wally signals.

--- wally-pipelined/testbench/function_radix.sv
+++ wally-pipelined/testbench/function_radix.sv
@@ -1,6 +1,30 @@
-// Ross Thompson
-// November 05, 2019
-// Oklahoma State University
+///////////////////////////////////////////
+// datapath.sv
+//
+// Written: Ross Thompson
+// email: ross1728@gmail.com
+// Created: November 9, 2019
+//
+// Purpose: Finds the current function or global assembly label based on PCE.
+// 
+// A component of the Wally configurable RISC-V project.
+// 
+// Copyright (C) 2021 Harvey Mudd College & Oklahoma State University
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
+// modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
+// is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS 
+// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT 
+// OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+///////////////////////////////////////////
+
+`include "wally-config.vh"
 
 module function_radix();
 
