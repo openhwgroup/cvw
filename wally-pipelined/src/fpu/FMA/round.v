3c86d0912a7169d523298c9f9a98ae9737fdf5d7
Katherine Parry <kparry4@gmail.com>
2021-03-04 22:18:19 +0000

    fixed various bugs

--- wally-pipelined/src/fpu/FMA/round.v
+++ wally-pipelined/src/fpu/FMA/round.v
@@ -19,37 +19,37 @@ module round(v[53:0], earlyres[51:0], earlyressel, rz, rn, rp, rm, wsign,
 			  w[51:0], postnormalize, infinity, specialsel);
 /////////////////////////////////////////////////////////////////////////////
 
-	input		[53:0]		v;				// normalized sum, R, S bits
-	input		[51:0]		earlyres;		// result from other FPU blocks
-	input 					earlyressel; 	// use result from other FPU blocks
-	input					rz;				// Round toward zero
-	input					rn;				// Round toward	nearest
-	input					rp;				// Round toward	plus infinity
-	input					rm;				// Round toward	minus infinity
-	input					wsign;			// Sign of result
-	input 					invalid;		// Trap on infinity, NaN, denorm
-	input					overflow;		// Result overflowed
-	input					underflow;		// Result underflowed
-	input					inf;			// Some input is infinity
-	input					nan;			// Some input is NaN
-	input					xnan;			// X is NaN
-	input					ynan;			// Y is NaN
-	input					znan;			// Z is NaN
-	input		[51:0]		x;				// Input X
-	input		[51:0]		y;				// Input Y
-	input		[51:0]		z;				// Input Z
-	output		[51:0]		w; 				// rounded result of FMAC
-	output					postnormalize; 	// Right shift 1 for post-rounding norm
-	output					infinity;    	// Generate infinity on overflow
-	output					specialsel;  	// Select special result
+	input		[53:0]		v;		// normalized sum, R, S bits
+	input		[51:0]		earlyres;	// result from other FPU blocks
+	input 				earlyressel; 	// use result from other FPU blocks
+	input				rz;		// Round toward zero
+	input				rn;		// Round toward	nearest
+	input				rp;		// Round toward	plus infinity
+	input				rm;		// Round toward	minus infinity
+	input				wsign;		// Sign of result
+	input 				invalid;	// Trap on infinity, NaN, denorm
+	input				overflow;	// Result overflowed
+	input				underflow;	// Result underflowed
+	input				inf;		// Some input is infinity
+	input				nan;		// Some input is NaN
+	input				xnan;		// X is NaN
+	input				ynan;		// Y is NaN
+	input				znan;		// Z is NaN
+	input		[51:0]		x;		// Input X
+	input		[51:0]		y;		// Input Y
+	input		[51:0]		z;		// Input Z
+	output		[51:0]		w; 		// rounded result of FMAC
+	output				postnormalize; 	// Right shift 1 for post-rounding norm
+	output				infinity;    	// Generate infinity on overflow
+	output				specialsel;  	// Select special result
 
 	// Internal nodes
 
-	wire					plus1;			// Round by adding one 
-	wire		[52:0]		v1;				// Result + 1 (for rounding)
-	wire		[51:0]		specialres;		// Result of exceptional case 
+	wire				plus1;		// Round by adding one 
+	wire		[52:0]		v1;		// Result + 1 (for rounding)
+	wire		[51:0]		specialres;	// Result of exceptional case 
 	wire		[51:0]		infinityres;	// Infinity or largest real number
-	wire		[51:0]		nanres;			// Propagated or generated NaN 
+	wire		[51:0]		nanres;		// Propagated or generated NaN 
 
 	// Compute if round should occur.  This equation is derived from
 	// the rounding tables.
@@ -77,7 +77,7 @@ module round(v[53:0], earlyres[51:0], earlyressel, rz, rn, rp, rm, wsign,
 	assign specialsel = earlyressel || overflow || underflow || invalid ||
 							nan || inf;
 	assign specialres = earlyressel ? earlyres : 
-						 invalid ? nanres : 
+						 invalid | nan ? nanres : //KEP added nan
 						 overflow ? infinityres : 
 						 inf ? 52'b0 :
 						underflow ? 52'b0 : 52'bx;  // default to undefined 
@@ -93,6 +93,11 @@ module round(v[53:0], earlyres[51:0], earlyressel, rz, rn, rp, rm, wsign,
 	// NaN inputs are already quiet, we don't have to force them quiet.
 
 	// assign nanres = xnan ? x: (ynan ? y : (znan ? z : {1'b1, 51'b0})); // original
+
+	// IEEE 754-2008 section 6.2.3 states:
+	// "If two or more inputs are NaN, then the payload of the resulting NaN should be 
+	// identical to the payload of one of the input NaNs if representable in the destination
+	// format. This standard does not specify which of the input NaNs will provide the payload."
 	assign nanres = xnan ? {1'b1, x[50:0]}: (ynan ? {1'b1, y[50:0]} : (znan ? {1'b1, z[50:0]} : {1'b1, 51'b0}));// KEP 210112 add the 1 to make NaNs quiet
 
 	// Select result with 4:1 mux
