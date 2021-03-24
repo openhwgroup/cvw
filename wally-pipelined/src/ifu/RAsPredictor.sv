84ad1353e4461fd5a8cffd08794a20c975e1d629
Ross Thompson <stephen.thompson.37@us.af.mil>
2021-03-23 21:49:16 -0500

    Fixed a bunch of bugs with the RAS.

--- wally-pipelined/src/ifu/RAsPredictor.sv
+++ wally-pipelined/src/ifu/RAsPredictor.sv
@@ -42,9 +42,9 @@ module RASPredictor
   logic 		    CounterEn;
   localparam Depth = $clog2(StackSize);
 
-  logic [StackSize-1:0]     PtrD, PtrQ, PtrP1, PtrM1;
-  logic [StackSize-1:0] [`XLEN-1:0] memory;
-  integer 			    index;
+  logic [Depth-1:0]     PtrD, PtrQ, PtrP1, PtrM1;
+  logic [Depth-1:0] [`XLEN-1:0] memory;
+  integer 			index;
   
   assign CounterEn = pop | push | incr;
 
