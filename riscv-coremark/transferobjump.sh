0fe798d5e1aed0345d7f9831ff0989b379f0c40a
Elizabeth Hedenberg <ehedenberg@g.hmc.edu>
2021-05-06 17:45:53 -0400

    pushing coremark to main branch

---
+++ riscv-coremark/transferobjump.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+./build-coremark.sh
+
+riscv64-unknown-elf-objdump -D coremark.bare.riscv > coremarkcodemod.bare.riscv.objdump 
+cp coremarkcodemod.bare.riscv.objdump ~/riscv-wally/imperas-riscv-tests/riscv-ovpsim-plus/examples/CoreMark/.
+pushd ~/riscv-wally/imperas-riscv-tests/riscv-ovpsim-plus/examples/CoreMark
+./exe2memfile.pl coremarkcodemod.bare.riscv
+popd
