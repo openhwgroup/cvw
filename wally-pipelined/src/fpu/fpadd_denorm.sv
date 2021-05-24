6f38b7633c558d680925c194487f5012bb6ac334
James E. Stine <james.stine@okstate.edu>
2021-05-24 08:28:16 -0500

    Update header for FPadd

--- wally-pipelined/src/fpu/fpadd_denorm.sv
+++ wally-pipelined/src/fpu/fpadd_denorm.sv
@@ -1,14 +1,27 @@
+///////////////////////////////////////////
 //
-// File name : fpadd
-// Title     : Floating-Point Adder/Subtractor
-// project   : FPU
-// Library   : fpadd
-// Author(s) : James E. Stine, Jr., Brett Mathis
-// Purpose   : definition of main unit to floating-point add/sub
-// notes :   
+// Written: James.Stine@okstate.edu 1 February 2021
+// Modified: 
 //
-// Copyright Oklahoma State University
-// Copyright AFRL
+// Purpose: FP Add/Sub instructions
+// 
+// A component of the Wally configurable RISC-V project.
+// 
+// Copyright (C) 2021 Harvey Mudd College & Oklahoma State University
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
+// modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
+// is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS 
+// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT 
+// OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+///////////////////////////////////////////
+
 //
 // Basic and Denormalized Operations
 //
@@ -25,7 +38,6 @@
 // Step 7: Round the result.// 
 // Step 8: Put sum onto output.
 //
-
 
 module fpadd (AS_Result, Flags, Denorm, op1, op2, rm, op_type, P, OvEn, UnEn);
 
