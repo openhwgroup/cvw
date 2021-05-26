3869a73a9cb3faa64582ae00641a610b98698418
Katherine Parry <kparry4@gmail.com>
2021-05-25 20:04:34 -0400

    renamed top level FPU wires

--- wally-pipelined/src/fpu/special.sv
+++ wally-pipelined/src/fpu/special.sv
@@ -10,46 +10,46 @@
 /////////////////////////////////////////////////////////////////////////////
 
 /////////////////////////////////////////////////////////////////////////////
-module special(Input1E, Input2E, Input3E, xzeroE, yzeroE, zzeroE,
+module special(FInput1E, FInput2E, FInput3E, xzeroE, yzeroE, zzeroE,
 				xnanE, ynanE, znanE, xdenormE, ydenormE, zdenormE, xinfE, yinfE, zinfE);
 /////////////////////////////////////////////////////////////////////////////
 
-	input logic   	[63:0]     	Input1E;              // Input Input1E
-	input logic     	[63:0]     	Input2E;           	// Input Input2E
-	input logic      	[63:0]    	Input3E;            	// Input Input3E 
-	output logic				xzeroE;		// Input Input1E = 0
-	output logic				yzeroE;		// Input Input2E = 0
-	output logic				zzeroE;		// Input Input3E = 0
-	output logic				xnanE;		// Input1E is NaN
-	output logic				ynanE;		// Input2E is NaN
-	output logic				znanE;		// Input3E is NaN
-	output logic				xdenormE;	// Input1E is denormalized
-	output logic				ydenormE;	// Input2E is denormalized
-	output logic				zdenormE;	// Input3E is denormalized
-	output logic				xinfE;		// Input1E is infinity
-	output logic				yinfE;		// Input2E is infinity
-	output logic				zinfE;		// Input3E is infinity
+	input logic   	[63:0]     	FInput1E;              // Input FInput1E
+	input logic     	[63:0]     	FInput2E;           	// Input FInput2E
+	input logic      	[63:0]    	FInput3E;            	// Input FInput3E 
+	output logic				xzeroE;		// Input FInput1E = 0
+	output logic				yzeroE;		// Input FInput2E = 0
+	output logic				zzeroE;		// Input FInput3E = 0
+	output logic				xnanE;		// FInput1E is NaN
+	output logic				ynanE;		// FInput2E is NaN
+	output logic				znanE;		// FInput3E is NaN
+	output logic				xdenormE;	// FInput1E is denormalized
+	output logic				ydenormE;	// FInput2E is denormalized
+	output logic				zdenormE;	// FInput3E is denormalized
+	output logic				xinfE;		// FInput1E is infinity
+	output logic				yinfE;		// FInput2E is infinity
+	output logic				zinfE;		// FInput3E is infinity
 
 	// In the actual circuit design, the gates looking at bits
 	// 51:0 and at bits 62:52 should be shared among the various detectors.
 
 	// Check if input is NaN
 
-	assign xnanE = &Input1E[62:52] && |Input1E[51:0]; 
-	assign ynanE = &Input2E[62:52] && |Input2E[51:0]; 
-	assign znanE = &Input3E[62:52] && |Input3E[51:0];
+	assign xnanE = &FInput1E[62:52] && |FInput1E[51:0]; 
+	assign ynanE = &FInput2E[62:52] && |FInput2E[51:0]; 
+	assign znanE = &FInput3E[62:52] && |FInput3E[51:0];
 
 	// Check if input is denormalized
 
-	assign xdenormE = ~(|Input1E[62:52]) && |Input1E[51:0]; 
-	assign ydenormE = ~(|Input2E[62:52]) && |Input2E[51:0]; 
-	assign zdenormE = ~(|Input3E[62:52]) && |Input3E[51:0];
+	assign xdenormE = ~(|FInput1E[62:52]) && |FInput1E[51:0]; 
+	assign ydenormE = ~(|FInput2E[62:52]) && |FInput2E[51:0]; 
+	assign zdenormE = ~(|FInput3E[62:52]) && |FInput3E[51:0];
 
 	// Check if input is infinity
 
-	assign xinfE = &Input1E[62:52] && ~(|Input1E[51:0]); 
-	assign yinfE = &Input2E[62:52] && ~(|Input2E[51:0]); 
-	assign zinfE = &Input3E[62:52] && ~(|Input3E[51:0]);
+	assign xinfE = &FInput1E[62:52] && ~(|FInput1E[51:0]); 
+	assign yinfE = &FInput2E[62:52] && ~(|FInput2E[51:0]); 
+	assign zinfE = &FInput3E[62:52] && ~(|FInput3E[51:0]);
 
 	// Check if inputs are all zero
 	// Also forces denormalized inputs to zero.
@@ -57,11 +57,11 @@ module special(Input1E, Input2E, Input3E, xzeroE, yzeroE, zzeroE,
 	// to just check if the exponent is zero.
 	
 	// KATHERINE - commented following (21/01/11)
-	// assign xzeroE = ~(|Input1E[62:0]) || xdenormE;
-	// assign yzeroE = ~(|Input2E[62:0]) || ydenormE;
-	// assign zzeroE = ~(|Input3E[62:0]) || zdenormE;
+	// assign xzeroE = ~(|FInput1E[62:0]) || xdenormE;
+	// assign yzeroE = ~(|FInput2E[62:0]) || ydenormE;
+	// assign zzeroE = ~(|FInput3E[62:0]) || zdenormE;
 	// KATHERINE - removed denorm to prevent output logicing zero when computing with a denormalized number
-	assign xzeroE = ~(|Input1E[62:0]);
-	assign yzeroE = ~(|Input2E[62:0]);
-	assign zzeroE = ~(|Input3E[62:0]);
+	assign xzeroE = ~(|FInput1E[62:0]);
+	assign yzeroE = ~(|FInput2E[62:0]);
+	assign zzeroE = ~(|FInput3E[62:0]);
  endmodule
