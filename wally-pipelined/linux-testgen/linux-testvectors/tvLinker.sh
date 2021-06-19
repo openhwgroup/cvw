10ca2ac5bcb40d4ce534a805aa4cf692915e823a
bbracker <bbracker@hmc.edu>
2021-06-18 22:01:18 -0400

    on-Tera solution for sym linking to linux testvectors

---
+++ wally-pipelined/linux-testgen/linux-testvectors/tvLinker.sh
@@ -0,0 +1,7 @@
+echo "Warning: this script will only work if your repo is on Tera"
+ln -s /courses/e190ax/buildroot_boot/parsedCSRs.txt parsedCSRs.txt
+ln -s /courses/e190ax/buildroot_boot/parsedMemRead.txt parsedMemRead.txt
+ln -s /courses/e190ax/buildroot_boot/parsedMemWrite.txt parsedMemWrite.txt
+ln -s /courses/e190ax/buildroot_boot/parsedPC.txt parsedPC.txt
+ln -s /courses/e190ax/buildroot_boot/parsedRegs.txt parsedRegs.txt
+echo "Done!"
