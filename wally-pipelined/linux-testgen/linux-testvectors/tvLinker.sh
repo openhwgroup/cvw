a3eafc6e5b252068c0ff2820ae4130c2751ce357
bbracker <bbracker@hmc.edu>
2021-06-18 22:28:07 -0400

    change buildroot config to use relative path for testvectors

--- wally-pipelined/linux-testgen/linux-testvectors/tvLinker.sh
+++ wally-pipelined/linux-testgen/linux-testvectors/tvLinker.sh
@@ -4,4 +4,6 @@ ln -s /courses/e190ax/buildroot_boot/parsedMemRead.txt parsedMemRead.txt
 ln -s /courses/e190ax/buildroot_boot/parsedMemWrite.txt parsedMemWrite.txt
 ln -s /courses/e190ax/buildroot_boot/parsedPC.txt parsedPC.txt
 ln -s /courses/e190ax/buildroot_boot/parsedRegs.txt parsedRegs.txt
+ln -s /courses/e190ax/buildroot_boot/bootmem.txt bootmem.txt
+ln -s /courses/e190ax/buildroot_boot/ram.txt ram.txt
 echo "Done!"
