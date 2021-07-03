d44916dacf691c33b8e171b34a1d5bfe362ba273
David Harris <david_harris@hmc.edu>
2021-07-03 02:25:31 -0400

    Cleaned up PMA/PMP checker unused code

--- wally-pipelined/src/mmu/pmpadrdec.sv
+++ wally-pipelined/src/mmu/pmpadrdec.sv
@@ -31,7 +31,6 @@
 
 module pmpadrdec (
   input  logic [`PA_BITS-1:0]      PhysicalAddress,
-//  input  logic [31:0]      HADDR, // *** replace with PAdr
   input  logic [1:0]       AdrMode,
   input  logic [`XLEN-1:0] CurrentPMPAdr,
   input  logic             AdrAtLeastPreviousPMP,
@@ -46,20 +45,19 @@ module pmpadrdec (
   logic TORMatch, NAMatch;
   logic AdrBelowCurrentPMP;
   logic [`PA_BITS-1:0] CurrentAdrFull;
-//  logic [`PA_BITS-1:0] FakePhysAdr; 
 
-  // ***replace this when the true physical address from MMU is available
-//  assign FakePhysAdr = {{(`PA_BITS-32){1'b0}}, HADDR};
+  // The two lsb of the physical address don't matter for this checking.
+  // The following code includes them, but hardwires the PMP checker lsbs to 00
+  // and masks them later.  Logic synthesis should optimize away these bottom bits.
  
   // Top-of-range (TOR)
   // Append two implicit trailing 0's to PMPAdr value
   assign CurrentAdrFull  = {CurrentPMPAdr[`PA_BITS-3:0],  2'b00};
-  assign AdrBelowCurrentPMP = PhysicalAddress < CurrentAdrFull; // *** make sure unsigned comparison works correctly
+  assign AdrBelowCurrentPMP = {1'b0, PhysicalAddress} < {1'b0, CurrentAdrFull}; // unsigned comparison
   assign AdrAtLeastCurrentPMP = ~AdrBelowCurrentPMP;
   assign TORMatch = AdrAtLeastPreviousPMP && AdrBelowCurrentPMP;
 
   // Naturally aligned regions
-  // *** should be able to optimize away bottom 2 bits
 
   // verilator lint_off UNOPTFLAT
   logic [`PA_BITS-1:0] Mask;
@@ -75,60 +73,6 @@ module pmpadrdec (
   // verilator lint_on UNOPTFLAT
 
   assign NAMatch = &((PhysicalAddress ~^ CurrentAdrFull) | Mask);
-
- /* generate
-    if (`XLEN == 32 || `XLEN == 64) begin // ***redo for various sizes
-      // priority encoder to translate address to range
-      // *** We'd like to replace this with a better priority encoder
-      // *** We should not be truncating 64 bit physical addresses to 32 bits...
-      // *** there is an easy combinatinoal way to do this with a cascade of AND gates O(32) rather than O(32^2) dh
-      always_comb
-        if (AdrMode == NA4) Range = (2**2) - 1;
-        else casez (CurrentPMPAdr[31:0]) // NAPOT regions
-          32'b???????????????????????????????0: Range = (2**3)  - 1;
-          32'b??????????????????????????????01: Range = (2**4)  - 1;
-          32'b?????????????????????????????011: Range = (2**5)  - 1;
-          32'b????????????????????????????0111: Range = (2**6)  - 1;
-          32'b???????????????????????????01111: Range = (2**7)  - 1;
-          32'b??????????????????????????011111: Range = (2**8)  - 1;
-          32'b?????????????????????????0111111: Range = (2**9)  - 1;
-          32'b????????????????????????01111111: Range = (2**10) - 1;
-          32'b???????????????????????011111111: Range = (2**11) - 1;
-          32'b??????????????????????0111111111: Range = (2**12) - 1;
-          32'b?????????????????????01111111111: Range = (2**13) - 1;
-          32'b????????????????????011111111111: Range = (2**14) - 1;
-          32'b???????????????????0111111111111: Range = (2**15) - 1;
-          32'b??????????????????01111111111111: Range = (2**16) - 1;
-          32'b?????????????????011111111111111: Range = (2**17) - 1;
-          32'b????????????????0111111111111111: Range = (2**18) - 1;
-          32'b???????????????01111111111111111: Range = (2**19) - 1;
-          32'b??????????????011111111111111111: Range = (2**20) - 1;
-          32'b?????????????0111111111111111111: Range = (2**21) - 1;
-          32'b????????????01111111111111111111: Range = (2**22) - 1;
-          32'b???????????011111111111111111111: Range = (2**23) - 1;
-          32'b??????????0111111111111111111111: Range = (2**24) - 1;
-          32'b?????????01111111111111111111111: Range = (2**25) - 1;
-          32'b????????011111111111111111111111: Range = (2**26) - 1;
-          32'b???????0111111111111111111111111: Range = (2**27) - 1;
-          32'b??????01111111111111111111111111: Range = (2**28) - 1;
-          32'b?????011111111111111111111111111: Range = (2**29) - 1;
-          32'b????0111111111111111111111111111: Range = (2**30) - 1;
-          32'b???01111111111111111111111111111: Range = (2**31) - 1;
-          32'b??011111111111111111111111111111: Range = (2**32) - 1;
-          32'b?0111111111111111111111111111111: Range = (2**33) - 1;
-          32'b01111111111111111111111111111111: Range = (2**34) - 1;
-          32'b11111111111111111111111111111111: Range = (2**35) - 1;
-          default:                              Range = '0;
-        endcase
-    end else begin
-      assign Range = '0;
-    end
-  endgenerate 
-
-  // *** Range should not be truncated... but our physical address space is
-  // currently only 32 bits wide.
-  // with a bit of combining of range selection,  this could be shared with NA4Match ***
-   assign NAMatch = &((HADDR ~^ CurrentAdrFull) | Range[31:0]);*/
 
   assign Match = (AdrMode == TOR) ? TORMatch : 
                  (AdrMode == NA4 || AdrMode == NAPOT) ? NAMatch :
