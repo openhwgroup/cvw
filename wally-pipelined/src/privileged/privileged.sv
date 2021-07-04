b5df9b282ddf4211ad5b77e4933a7d8ae62c7eaa
David Harris <david_harris@hmc.edu>
2021-07-04 11:39:59 -0400

    Added support for PMP lock bits in csrm and repartitioned design to pass around 8-bit PMPCFG entries

--- wally-pipelined/src/privileged/privileged.sv
+++ wally-pipelined/src/privileged/privileged.sv
@@ -64,11 +64,11 @@ module privileged (
   input logic PMALoadAccessFaultM, PMPLoadAccessFaultM,
   input logic PMAStoreAccessFaultM, PMPStoreAccessFaultM,
 
-  output logic		   IllegalFPUInstrE,
+  output logic		         IllegalFPUInstrE,
   output logic [1:0]       PrivilegeModeW,
   output logic [`XLEN-1:0] SATP_REGW,
   output logic             STATUS_MXR, STATUS_SUM,
-  output var logic [63:0]      PMPCFG_ARRAY_REGW[`PMP_ENTRIES/8-1:0],
+  output var logic [7:0]   PMPCFG_ARRAY_REGW[`PMP_ENTRIES-1:0],
   output var logic [`XLEN-1:0] PMPADDR_ARRAY_REGW [`PMP_ENTRIES-1:0], 
   output logic [2:0]       FRM_REGW
 );
