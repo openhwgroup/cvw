4d53a935b3eea276972d79e49fa78c72872265fd
David Harris <david_harris@hmc.edu>
2021-07-08 20:08:04 -0400

    Fixed missing stall in InstrRet counter

--- wally-pipelined/src/mmu/tlblru.sv
+++ wally-pipelined/src/mmu/tlblru.sv
@@ -48,5 +48,5 @@ module tlblru #(parameter TLB_ENTRIES = 8) (
   assign AllUsed = &RUBitsAccessed; // if all recently used, then clear to none
   assign RUBitsNext = AllUsed ? 0 : RUBitsAccessed; 
   flopenrc #(TLB_ENTRIES) lrustate(clk, reset, TLBFlush, (CAMHit || TLBWrite), RUBitsNext, RUBits);
-
+  // *** seems like enable must be ORd with TLBFlush to ensure flop fires on a flush.  DH 7/8/21
 endmodule
