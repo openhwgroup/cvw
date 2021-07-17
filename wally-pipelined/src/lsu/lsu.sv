fc88b3a69306e8273c91bbfc0e106c96772bc077
David Harris <david_harris@hmc.edu>
2021-07-17 19:09:13 -0400

    Continued Translation Address Cleanup

--- wally-pipelined/src/lsu/lsu.sv
+++ wally-pipelined/src/lsu/lsu.sv
@@ -165,8 +165,6 @@ module lsu
 				  .DTLBWriteM(DTLBWriteM),
 				  .HPTWReadPTE(HPTWReadPTE),
 				  .HPTWStall(HPTWStall),
-//				  .HPTWPAdrE(HPTWPAdrE),
-//				  .HPTWPAdrM(HPTWPAdrM),	
           .TranslationVAdr,
           .TranslationPAdr,			  
           .UseTranslationVAdr,
@@ -182,8 +180,7 @@ module lsu
     else             assign TranslationPAdrXLEN = {{(`XLEN-`PA_BITS){1'b0}}, TranslationPAdr[`PA_BITS-1:0]};
   endgenerate
   mux2 #(`XLEN) HPTWPAdrMux(TranslationPAdrXLEN, TranslationVAdr, UseTranslationVAdr, HPTWPAdrE); // *** misleading to call it PAdr, bad because some bits have been truncated
-  flop #(`XLEN) HPTWPAdrMReg(clk, HPTWPAdrE, HPTWPAdrM);  // *** perhaps HPTW should just send PAdrE, and LSU can latch it as necessary
-
+  flop #(`XLEN) HPTWPAdrMReg(clk, HPTWPAdrE, HPTWPAdrM);  
   
   assign WalkerPageFaultM = WalkerStorePageFaultM | WalkerLoadPageFaultM;
 
