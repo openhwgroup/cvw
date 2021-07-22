ae29eaa98dfc86363ad0cf5f262e12360975a0e4
David Harris <david_harris@hmc.edu>
2021-07-22 12:30:46 -0400

    Simplified interface to fclassify and fsgn

--- wally-pipelined/src/fpu/fclassify.sv
+++ wally-pipelined/src/fpu/fclassify.sv
@@ -3,43 +3,18 @@
 
 module fclassify (
     input  logic XSgnE,
-    input  logic [51:0] XFracE,
     input logic XNaNE, 
     input logic XSNaNE,
     input logic XNormE,
     input logic XDenormE,
     input logic XZeroE,
     input logic XInfE,
-    // input  logic        FmtE,           // 0-Single 1-Double
     output logic [63:0] ClassResE
     );
 
-    // logic XSgnE;
-    // logic Inf, NaN, Zero, Norm, Denorm;
     logic PInf, PZero, PNorm, PDenorm;
     logic NInf, NZero, NNorm, NDenorm;
-    // logic MaxExp, ExpZero, ManZero, FirstBitFrac;
-   
-    // Single and Double precision layouts
-    // assign XSgnE = FmtE ? FSrcXE[63] : FSrcXE[31];
 
-    // basic calculations for readabillity
-    
-    // assign ExpZero = FmtE ? ~|FSrcXE[62:52] : ~|FSrcXE[30:23];
-    // assign MaxExp = FmtE ? &FSrcXE[62:52] : &FSrcXE[30:23];
-    // assign ManZero = FmtE ? ~|FSrcXE[51:0] : ~|FSrcXE[22:0];
-    // assign FirstBitFrac = FmtE ? FSrcXE[51] : FSrcXE[22];
-
-    // determine the type of number
-    // assign NaN      = MaxExp & ~ManZero;
-    // assign Inf = MaxExp & ManZero;
-    // assign Zero     = ExpZero & ManZero;
-    // assign Denorm= ExpZero & ~ManZero;
-    // assign Norm   = ~ExpZero;
-
-    // determine the sub categories
-    // assign QNaN = FirstBitFrac&NaN;
-    // assign SNaN = ~FirstBitFrac&NaN;
     assign PInf = ~XSgnE&XInfE;
     assign NInf = XSgnE&XInfE;
     assign PNorm = ~XSgnE&XNormE;
