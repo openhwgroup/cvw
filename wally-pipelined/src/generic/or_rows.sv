118cb7fb87ef89fc0b2eb63f2c4a85cc30b54967
David Harris <david_harris@hmc.edu>
2021-09-08 15:59:40 -0400

    Added testbench-arch for riscv-arch-test suite

--- wally-pipelined/src/generic/or_rows.sv
+++ wally-pipelined/src/generic/or_rows.sv
@@ -33,23 +33,14 @@ module or_rows #(parameter ROWS = 8, COLS=2) (
   input  var logic [COLS-1:0] a[ROWS-1:0],
   output logic [COLS-1:0] y); 
 
-  logic [COLS-1:0] mid[ROWS-1:0];
+  logic [COLS-1:0] mid[ROWS-1:1];
   genvar row, col;
   generate
     assign mid[1] = a[0] | a[1];
     for (row=2; row < ROWS; row++)
       assign mid[row] = mid[row-1] | a[row];
-    assign y = mid[ROWS-1];
-    
-    /*
-      for (col = 0; col < COLS; col++) begin
-          assign mid[1][col] = a[0][col] | a[1][col];
-          for (row=2; row < ROWS; row++)
-            assign mid[row][col] = mid[row-1][col] | a[row][col];
-          assign y[col] = mid[ROWS-1][col];
-      end
-      */
-  endgenerate
+    assign y = mid[ROWS-1];   
+   endgenerate
 endmodule
 
 /* verilator lint_on UNOPTFLAT */
