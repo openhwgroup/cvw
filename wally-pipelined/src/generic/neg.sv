42d573be579bd178b9b5f05ecc0275841d7aad53
David Harris <david_harris@hmc.edu>
2021-09-30 12:17:24 -0400

    SRT Division unsigned passing Imperas tests

---
+++ wally-pipelined/src/generic/neg.sv
@@ -0,0 +1,34 @@
+///////////////////////////////////////////
+// neg.sv
+//
+// Written: David_Harris@hmc.edu 28 September 2021
+// Modified: 
+//
+// Purpose: 2's complement negator
+// 
+// A component of the Wally configurable RISC-V project.
+// 
+// Copyright (C) 2021 Harvey Mudd College & Oklahoma State University
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
+// modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
+// is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS 
+// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT 
+// OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+///////////////////////////////////////////
+
+`include "wally-config.vh"
+
+module neg #(parameter WIDTH = 8) (
+  input  logic [WIDTH-1:0] a,
+  output logic [WIDTH-1:0] y);
+
+  assign y = ~a + 1;
+endmodule
+
