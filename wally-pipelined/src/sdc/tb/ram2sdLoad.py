b3694bfdfd285df7c1cfcebe0bf9d8a0403f2bfc
Ross Thompson <ross1728@gmail.com>
2021-10-11 17:22:23 -0500

    Fixed boot loader program to start at correct address. modified script which converts the ram.txt into preload text file for sdc simulation. created script to convert ram.txt into binary to write to flash card. added top level for solo sd card fpga.

---
+++ wally-pipelined/src/sdc/tb/ram2sdLoad.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import sys, fileinput
+
+address = 0
+
+
+for line in fileinput.input('-'):
+    # the 14- is to reverse the byte order to little endian
+    formatedLine = ' '.join(line[14-i:14-i+2] for i in range(0, len(line), 2))
+    sys.stdout.write('@{:08x} {:s}\n'.format(address, formatedLine))
+    address+=8
