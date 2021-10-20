0e4f6392d61e0c3ec4283c3189292dc53101195a
David Harris <david_harris@hmc.edu>
2021-10-20 09:03:21 -0700

    Move tests into subdirectory and moved wavedrom out of project

---
+++ tests/testsBP/simple/main.c
@@ -0,0 +1,21 @@
+#include "header.h"
+
+int main(){
+  //int res = icache_spill_test();
+  global_hist_3_space_test();  
+  global_hist_2_space_test();
+  global_hist_1_space_test();
+  global_hist_0_space_test();    
+  int res = 1;
+  if (res < 0) {
+    fail();
+    return 0;
+  }else {
+    if((res = lbu_test()) < 0) {
+      fail();
+      return 0;
+    }
+    res = simple_csrbr_test();
+    return 0;
+  }
+}
