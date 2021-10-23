3249d65209b6cfff1d53f2277fe79f293ddc87de
David Harris <david_harris@hmc.edu>
2021-10-23 06:15:26 -0700

    Added -lint flag to vsim.  Cleaned some lint errors.  Moved lint-wally to regression directory for convenience.

--- wally-pipelined/regression/regression-wally.py
+++ wally-pipelined/regression/regression-wally.py
@@ -35,7 +35,7 @@ configs = [
     ),
     TestCase(
         name="lints",
-        cmd="../lint-wally &> {}",
+        cmd="./lint-wally &> {}",
         grepstr="All lints run with no errors or warnings"
     ),
 ]
