09959617c6b54d3a92e1d94feedce6a38066181c
bbracker <bbracker@hmc.edu>
2021-10-24 15:18:44 -0700

    update debugger script to new style

---
+++ wally-pipelined/linux-testgen/testvector-generation/debug.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+source genSettings.sh
+tcpPort=1237
+
+# Run without GDB
+($customQemu \
+-M virt \
+-nographic \
+-bios $imageDir/fw_jump.elf -kernel $imageDir/Image -append "root=/dev/vda ro" -initrd $imageDir/rootfs.cpio \
+-singlestep -rtc clock=vm -icount shift=1,align=off,sleep=on)
+
+# Run with GDB
+#($customQemu \
+#-M virt \
+#-nographic -serial /dev/null \
+#-bios $imageDir/fw_jump.elf -kernel $imageDir/Image -append "root=/dev/vda ro" -initrd $imageDir/rootfs.cpio \
+#-singlestep -rtc clock=vm -icount shift=1,align=off,sleep=on \
+#-gdb tcp::$tcpPort -S) \
+#& riscv64-unknown-elf-gdb -x debug.gdb -ex "debug $tcpPort"
+
