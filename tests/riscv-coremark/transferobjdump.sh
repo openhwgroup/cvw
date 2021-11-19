bdc212cf882dc95dc67519234216fed486220128
David Harris <david_harris@hmc.edu>
2021-11-18 17:15:40 -0800

    Simplifying riscv-coremark

--- tests/riscv-coremark/transferobjdump.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-./build-coremark.sh
-
-riscv64-unknown-elf-objdump -D coremark.bare.riscv > coremarkcodemod.bare.riscv.objdump 
-cp coremarkcodemod.bare.riscv.objdump ~/riscv-wally/tests/imperas-riscv-tests/riscv-ovpsim-plus/examples/CoreMark/.
-pushd ~/riscv-wally/tests/imperas-riscv-tests/riscv-ovpsim-plus/examples/CoreMark
-./exe2memfile.pl coremarkcodemod.bare.riscv
-popd
