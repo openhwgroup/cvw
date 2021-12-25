e97e512da916988c737e76150f61240fa48405b5
David Harris <david_harris@hmc.edu>
2021-12-25 22:39:51 +0000

    Started FIR test code and started incorporating Imperas tests

--- wally-pipelined/testbench/testbench.sv
+++ wally-pipelined/testbench/testbench.sv
@@ -286,13 +286,15 @@ logic [3:0] dummy;
   end
 
   // Termination condition
-  // terminate on a specific ECALL for Imperas tests, or on a jump to self infinite loop for RISC-V Arch tests
+  // terminate on a specific ECALL after li x3,1 for old Imperas tests, 
+  // or sw	gp,-56(t0) for new Imperas tests
+  // or on a jump to self infinite loop (6f) for RISC-V Arch tests
   assign DCacheFlushStart = dut.hart.priv.priv.EcallFaultM && 
 			    (dut.hart.ieu.dp.regf.rf[3] == 1 || 
 			     (dut.hart.ieu.dp.regf.we3 && 
 			      dut.hart.ieu.dp.regf.a3 == 3 && 
 			      dut.hart.ieu.dp.regf.wd3 == 1)) ||
-          dut.hart.ifu.InstrM == 32'h6f && dut.hart.ieu.c.InstrValidM;
+          (dut.hart.ifu.InstrM == 32'h6f || dut.hart.ifu.InstrM == 32'hfc32a423) && dut.hart.ieu.c.InstrValidM;
   
   DCacheFlushFSM DCacheFlushFSM(.clk(clk),
 				.reset(reset),
