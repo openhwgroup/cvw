13dbf3cc0f9201191b7f3bc3f304216ab87ac9f4
Ross Thompson <ross@flandre.ecen.okstate.edu>
2022-01-04 22:40:51 -0600

    parameterized the caches with the goal of using common rtl for both i and d caches.

--- pipelined/src/lsu/lsu.sv
+++ pipelined/src/lsu/lsu.sv
@@ -306,7 +306,9 @@ module lsu
 
   generate
     if(`MEM_DCACHE) begin : dcache
-      dcache dcache(.clk, .reset, .CPUBusy,
+      dcache #(.LINELEN(`DCACHE_LINELENINBITS), .NUMLINES(`DCACHE_WAYSIZEINBYTES*8/LINELEN),
+			   .NUMWAYS(`DCACHE_NUMWAYS)) 
+	  dcache(.clk, .reset, .CPUBusy,
             .LsuRWM, .FlushDCacheM, .LsuAtomicM, .LsuAdrE, .LsuPAdrM, .PreLsuPAdrM(PreLsuPAdrM[11:0]), // still don't like this name PreLsuPAdrM, not always physical
             .FinalWriteDataM, .ReadDataWordM, .DCacheStall,
             .DCacheMiss, .DCacheAccess, 
