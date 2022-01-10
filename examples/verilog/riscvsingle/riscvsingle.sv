2a7e77d2b181175af780d4caeccbf5152974a9b9
David Harris <david_harris@hmc.edu>
2022-01-10 16:26:18 +0000

    Do file for riscvsingle

--- examples/verilog/riscvsingle/riscvsingle.sv
+++ examples/verilog/riscvsingle/riscvsingle.sv
@@ -334,7 +334,7 @@ module imem(input  logic [31:0] a,
   logic [31:0] RAM[63:0];
 
   initial
-      $readmemh("riscvtest.txt",RAM);
+      $readmemh("riscvtest.memfile",RAM);
 
   assign rd = RAM[a[31:2]]; // word aligned
 endmodule
