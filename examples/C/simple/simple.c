486cfdc3a5688d5f82a84b6c0347690ab362c0ca
David Harris <david_harris@hmc.edu>
2022-01-11 21:01:48 +0000

    Added C test cases

--- examples/C/simple/simple.c
+++ examples/C/simple/simple.c
@@ -2,13 +2,20 @@
 // David_Harris@hmc.edu 24 December 2021
 // Simple illustration of compiling C code
 
+#include <stdio.h>
+
 long sum(long N) {
    long result, i;
    result = 0;
-   for (i=1; i<=N; i++) result = result + i;
+   for (i=1; i<=N; i++) {
+       result = result + i;
+   }
    return result;
 }
 
 int main(void) {
-    return sum(4);
+    long s;
+    s = sum(4);
+    printf("s = %ld\n", s);
+    return 0; // 0 means success
 }
