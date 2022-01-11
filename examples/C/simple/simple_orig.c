14e53099f8d33cac4635a8a5c45c9f3b99858d8a
James E. Stine <james.stine@okstate.edu>
2022-01-11 15:59:56 -0600

    Update on assembly simple/spike

--- examples/C/simple/simple_orig.c
+++ examples/C/simple/simple_orig.c
@@ -5,36 +5,31 @@
 //#include <stdio.h>
 #include "util.h"
 extern int printf(const char* fmt, ...);
-extern int _comp1(void);
 
-/*
 long sum(long N) {
-   long result, i;
+/*   long result, i;
    result = 0;
    for (i=1; i<=N; i++) {
        result = result + i;
    }
-   return result; 
+   return result; */
 
    int a;
-   asm volatile ("li s0, 10;");
-
+//   asm volatile ("li s0, 10;");
    asm volatile(
-	   "li %0, 3;"			
-	   //  "csrrs %0, 0xF14, zero" //CSSRS rd, mhartid, 0
-	   : "=r"(a) //output
-	   : //input
-	   : //clobbered
+           "li %0, 10"
+//	       "csrrs %0, 0xF14, zero" //CSSRS rd, mhartid, 0
+	       : "=r"(a) //output
+	       : //input
+	       : //clobbered
 		);
    return a;
 }
-*/
 
 int main(void) {
     int s[1], expected[1];
-    //    s[0] = sum(4);
-    s[0] = _comp1();
+    s[0] = sum(4);
     printf("s = %d\n", s[0]);
-    expected[0] = 42;
+    expected[0] = 10;
     return verify(1, s, expected); // 0 means success
 }
