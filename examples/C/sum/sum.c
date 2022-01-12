4b7ee9815eb9c93c50757eea9177174bc8f05ac2
David Harris <david_harris@hmc.edu>
2022-01-12 09:04:41 +0000

    C sum example

---
+++ examples/C/sum/sum.c
@@ -0,0 +1,23 @@
+// sum.C
+// David_Harris@hmc.edu 24 December 2021
+// Simple illustration of compiling C code
+
+#include <stdio.h>  // supports printf
+#include "util.h"   // supports verify
+
+long sum(long N) {
+  long result, i;
+   result = 0;
+   for (i=1; i<=N; i++) {
+       result = result + i;
+   }
+   return result;
+}
+
+int main(void) {
+    int s[1], expected[1];
+    s[0] = sum(4);
+    printf("s = %d\n", s[0]);
+    expected[0] = 10;
+    return verify(1, s, expected); // 0 means success
+}
