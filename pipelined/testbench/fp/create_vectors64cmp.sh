115ea7dbb07338ab559f093f0dd5b280a98dbd3b
James E. Stine <james.stine@okstate.edu>
2022-01-14 09:25:37 -0600

    Update to TestFloat for scripts so can run automatically once TestFloat/Softfloat is compiled.  Slight change to the README as well.

--- pipelined/testbench/fp/create_vectors64cmp.sh
+++ pipelined/testbench/fp/create_vectors64cmp.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
-./testfloat_gen f64_eq > f64_cmp_eq.tv
-./testfloat_gen f64_le > f64_cmp_le.tv
-./testfloat_gen f64_lt > f64_cmp_lt.tv
 
-./testfloat_gen f64_eq_signaling > f64_cmp_eq_signaling.tv
-./testfloat_gen f64_le_quiet > f64_cmp_le_quiet.tv
-./testfloat_gen f64_lt_quiet > f64_cmp_lt_quiet.tv
+BUILD="./TestFloat-3e/build/Linux-x86_64-GCC"
+OUTPUT="./vectors"
+
+$BUILD/testfloat_gen f64_eq > $OUTPUT/f64_cmp_eq.tv
+$BUILD/testfloat_gen f64_le > $OUTPUT/f64_cmp_le.tv
+$BUILD/testfloat_gen f64_lt > $OUTPUT/f64_cmp_lt.tv
+
+$BUILD/testfloat_gen f64_eq_signaling > $OUTPUT/f64_cmp_eq_signaling.tv
+$BUILD/testfloat_gen f64_le_quiet > $OUTPUT/f64_cmp_le_quiet.tv
+$BUILD/testfloat_gen f64_lt_quiet > $OUTPUT/f64_cmp_lt_quiet.tv
 
