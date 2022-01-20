3005d82dbac774b077c26522c2566b09704a049e
David Harris <david_harris@hmc.edu>
2022-01-20 00:04:23 +0000

    Created linux directory for linux config

--- examples/verilog/riscvsingle/riscvsingle.sv
+++ examples/verilog/riscvsingle/riscvsingle.sv
@@ -227,8 +227,8 @@ module controller(
       7'b0000011: controls = 11'b1_00_01_0_0_0_1_0_0; // lw
       7'b0100011: controls = 11'b0_01_01_0_0_1_0_0_0; // sw
       7'b0110011: controls = 11'b1_xx_00_1_0_0_0_0_0; // R-type 
-      7'b1100011: controls = 11'b0_10_11_0_0_0_0_1_0; // beq
       7'b0010011: controls = 11'b1_00_01_1_0_0_0_0_0; // I-type ALU
+      7'b1100011: controls = 11'b0_10_11_0_0_0_0_1_0; // beq
       7'b1101111: controls = 11'b1_11_11_0_1_0_0_0_1; // jal
       default:    controls = 11'bx_xx_xx_x_x_x_x_x_x; // non-implemented instruction
     endcase
