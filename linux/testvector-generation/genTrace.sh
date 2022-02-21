356993df7cb1b19755711315deb56dbc009b3f9f
bbracker <bbracker@hmc.edu>
2022-02-21 20:30:39 +0000

    new trace generation method

--- linux/testvector-generation/genTrace.sh
+++ linux/testvector-generation/genTrace.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 tcpPort=1234
 imageDir=$RISCV/buildroot/output/images
-outDir=$RISCV/linux-testvectors
-recordFile="$outDir/all.qemu"
-traceFile="$outDir/all.txt"
-interruptsFile="$outDir/interrupts.txt"
+tvDir=$RISCV/linux-testvectors
+recordFile="$tvDir/all.qemu"
+traceFile="$tvDir/all.txt"
+interruptsFile="$tvDir/interrupts.txt"
 
 read -p "Warning: running this script will overwrite the contents of:
-  * $recordFile
   * $traceFile
   * $interruptsFile
 Would you like to proceed? (y/n) " -n 1 -r
@@ -15,12 +14,11 @@ echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     # Create Output Directory
-    sudo mkdir -p $outDir
-    sudo chown cad $outDir
-    sudo touch $recordFile 
+    echo "Elevating permissions to create $traceFile, $interruptsFile"
+    sudo mkdir -p $tvDir
+    sudo chown cad $tvDir
     sudo touch $traceFile 
     sudo touch $interruptsFile 
-    sudo chmod a+rw $recordFile
     sudo chmod a+rw $traceFile
     sudo chmod a+rw $interruptsFile
 
@@ -28,21 +26,19 @@ then
     dtc -I dts -O dtb ../devicetree/wally-virt.dts > ../devicetree/wally-virt.dtb
 
     # QEMU Simulation
+    echo "Launching QEMU in replay mode!"
     (qemu-system-riscv64 \
     -M virt -dtb ../devicetree/wally-virt.dtb \
-    -nographic -serial /dev/null \
+    -nographic \
     -bios $imageDir/fw_jump.elf -kernel $imageDir/Image -append "root=/dev/vda ro" -initrd $imageDir/rootfs.cpio \
-    -singlestep -rtc clock=vm -icount shift=0,align=off,sleep=on,rr=record,rrfile=$recordFile \
+    -singlestep -rtc clock=vm -icount shift=0,align=off,sleep=on,rr=replay,rrfile=$recordFile \
     -d nochain,cpu,in_asm,int \
-    -gdb tcp::$tcpPort -S \
-    2>&1 >/dev/null | ./parseQemuToGDB.py | ./parseGDBtoTrace.py $interruptsFile | ./remove_dup.awk > $traceFile) \
-    & riscv64-unknown-elf-gdb -quiet -x genTrace.gdb -ex "genTrace $tcpPort \"$imageDir/vmlinux\""
+    2>&1 >/dev/null | ./parseQemuToGDB.py | ./parseGDBtoTrace.py $interruptsFile | ./remove_dup.awk > $traceFile)
 
     # Cleanup
-    sudo chown cad $recordFile
+    echo "Elevating permissions to restrict write access to $traceFile, $interruptsFile"
     sudo chown cad $traceFile
     sudo chown cad $interruptsFile
-    sudo chmod o-w $recordFile
     sudo chmod o-w $traceFile
     sudo chmod o-w $interruptsFile
 fi
