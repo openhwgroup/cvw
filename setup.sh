c13517f0cebc3df411f089049eec708d92a1f001
David Harris <david_harris@hmc.edu>
2022-03-03 21:00:07 -0800

    Defined WALLY in setup as pointer to repository

--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# wally-setup.sh
+# setup.sh
 # David_Harris@hmc.edu and kekim@hmc.edu 1 December 2021
 # Set up tools for riscv-wally
 
 echo "Executing Wally setup.sh"
+
+# Path to Wally repository
+WALLY=$(dirname ${BASH_SOURCE})
+export WALLY=$(cd "$WALLY" && pwd)
+echo \$WALLY set to ${WALLY}
 
 # Path to RISC-V Tools
 export RISCV=/opt/riscv   # change this if you installed the tools in a different location
@@ -16,8 +21,8 @@ export PATH=$PATH:$RISCV/riscv-gnu-toolchain/bin:$RISCV/riscv-gnu-toolchain/risc
 # Spike
 export LD_LIBRARY_PATH=$RISCV/lib:$LD_LIBRARY_PATH
 export PATH=$PATH:$RISCV/bin
-# exe2memfile
-export PATH=~/riscv-wally/bin:$PATH    # exe2memfile; change this if riscv-wally isn't at your home directory
+# utility functions in Wally repository
+export PATH=$WALLY/bin:$PATH    
 # Verilator
 export PATH=/usr/local/bin/verilator:$PATH # Change this for your path to Verilator
 # ModelSim/Questa (vsim)
@@ -26,6 +31,6 @@ export MGLS_LICENSE_FILE=1717@solidworks.eng.hmc.edu # Change this to your Sieme
 export PATH=/cad/synopsys/SYN/bin:$PATH  # Change this for your path to Design Compiler
 export SNPSLMD_LICENSE_FILE=27020@134.173.38.214
 
-# Imperas; *** remove if not using; *** fix paths
-export PATH=$RISCV/imperas-riscv-tests/riscv-ovpsim-plus/bin/Linux64:$PATH  # *** maybe take this out based on Imperas
-export LD_LIBRARY_PATH=$RISCV/imperas_riscv_tests/riscv-ovpsim-plus/bin/Linux64:$LD_LIBRARY_PATH # remove if no imperas
+# Imperas; put this in if you are using it
+#export PATH=$RISCV/imperas-riscv-tests/riscv-ovpsim-plus/bin/Linux64:$PATH  
+#export LD_LIBRARY_PATH=$RISCV/imperas_riscv_tests/riscv-ovpsim-plus/bin/Linux64:$LD_LIBRARY_PATH # remove if no imperas
