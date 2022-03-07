409dd4870672cdc5fffe4810c5922a2307f16cea
bbracker <bbracker@hmc.edu>
2022-03-07 11:56:04 -0800

    modify debug.sh to not rely on external GDB script

--- linux/testvector-generation/debug.sh
+++ linux/testvector-generation/debug.sh
@@ -9,6 +9,13 @@ tcpPort=1239
 -nographic \
 -bios $imageDir/fw_jump.elf -kernel $imageDir/Image -append "root=/dev/vda ro" -initrd $imageDir/rootfs.cpio \
 -singlestep -rtc clock=vm -icount shift=0,align=off,sleep=on \
-> ./qemu-serial) # \
-#-gdb tcp::$tcpPort -S \
-#& riscv64-unknown-elf-gdb -quiet -x debug.gdb -ex "debug $tcpPort \"$imageDir/vmlinux\""
+> ./qemu-serial \
+-gdb tcp::$tcpPort -S) \
+& riscv64-unknown-elf-gdb -quiet \
+-ex "set pagination off" \
+-ex "set logging overwrite on" \
+-ex "set logging redirect on" \
+-ex "set confirm off" \
+-ex "target extended-remote :$tcpPort" \
+-ex "maintenance packet Qqemu.PhyMemMode:1" \
+-ex "file $imageDir/vmlinux"
