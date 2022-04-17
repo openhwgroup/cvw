2819a1c305143bb63d0a3ee088881036dca890e7
David Harris <david_harris@hmc.edu>
2022-04-17 22:33:25 +0000

    Remvoed bytemask anding from FinalWriteDataM in subwordwrite

--- pipelined/src/lsu/subwordwrite.sv
+++ pipelined/src/lsu/subwordwrite.sv
@@ -36,52 +36,28 @@ module subwordwrite (
   input logic [`XLEN-1:0]    FinalAMOWriteDataM,
   output logic [`XLEN-1:0]   FinalWriteDataM,
   output logic [`XLEN/8-1:0] ByteMaskM
-                     );
-                  
-  logic [`XLEN-1:0]          WriteDataSubwordDuplicated;
+);
 
+  // Compute byte masks
   swbytemask swbytemask(.HSIZED({LSUFunct3M[2], 1'b0, LSUFunct3M[1:0]}), .HADDRD(LSUPAdrM),
     .ByteMask(ByteMaskM));
   
+  // Replicate data for subword writes
   if (`XLEN == 64) begin:sww
-    // Handle subword writes
     always_comb 
       case(LSUFunct3M[1:0])
-        2'b00:  WriteDataSubwordDuplicated = {8{FinalAMOWriteDataM[7:0]}};  // sb
-        2'b01:  WriteDataSubwordDuplicated = {4{FinalAMOWriteDataM[15:0]}}; // sh
-        2'b10:  WriteDataSubwordDuplicated = {2{FinalAMOWriteDataM[31:0]}}; // sw
-        2'b11:  WriteDataSubwordDuplicated = FinalAMOWriteDataM;            // sw
+        2'b00:  FinalWriteDataM = {8{FinalAMOWriteDataM[7:0]}};  // sb
+        2'b01:  FinalWriteDataM = {4{FinalAMOWriteDataM[15:0]}}; // sh
+        2'b10:  FinalWriteDataM = {2{FinalAMOWriteDataM[31:0]}}; // sw
+        2'b11:  FinalWriteDataM = FinalAMOWriteDataM;            // sw
       endcase
-
-    always_comb begin
-      FinalWriteDataM='0;
-      if (ByteMaskM[0]) FinalWriteDataM[7:0]   = WriteDataSubwordDuplicated[7:0];
-      if (ByteMaskM[1]) FinalWriteDataM[15:8]  = WriteDataSubwordDuplicated[15:8];
-      if (ByteMaskM[2]) FinalWriteDataM[23:16] = WriteDataSubwordDuplicated[23:16];
-      if (ByteMaskM[3]) FinalWriteDataM[31:24] = WriteDataSubwordDuplicated[31:24];
-      if (ByteMaskM[4]) FinalWriteDataM[39:32] = WriteDataSubwordDuplicated[39:32];
-      if (ByteMaskM[5]) FinalWriteDataM[47:40] = WriteDataSubwordDuplicated[47:40];
-      if (ByteMaskM[6]) FinalWriteDataM[55:48] = WriteDataSubwordDuplicated[55:48];
-      if (ByteMaskM[7]) FinalWriteDataM[63:56] = WriteDataSubwordDuplicated[63:56];
-    end 
-
   end else begin:sww // 32-bit
-    // Handle subword writes
     always_comb 
       case(LSUFunct3M[1:0])
-        2'b00:  WriteDataSubwordDuplicated = {4{FinalAMOWriteDataM[7:0]}};  // sb
-        2'b01:  WriteDataSubwordDuplicated = {2{FinalAMOWriteDataM[15:0]}}; // sh
-        2'b10:  WriteDataSubwordDuplicated = FinalAMOWriteDataM;            // sw
-        default: WriteDataSubwordDuplicated = FinalAMOWriteDataM; // shouldn't happen
+        2'b00:  FinalWriteDataM = {4{FinalAMOWriteDataM[7:0]}};  // sb
+        2'b01:  FinalWriteDataM = {2{FinalAMOWriteDataM[15:0]}}; // sh
+        2'b10:  FinalWriteDataM = FinalAMOWriteDataM;            // sw
+        default: FinalWriteDataM = FinalAMOWriteDataM; // shouldn't happen
       endcase
-
-    always_comb begin
-      FinalWriteDataM='0;
-      if (ByteMaskM[0]) FinalWriteDataM[7:0]   = WriteDataSubwordDuplicated[7:0];
-      if (ByteMaskM[1]) FinalWriteDataM[15:8]  = WriteDataSubwordDuplicated[15:8];
-      if (ByteMaskM[2]) FinalWriteDataM[23:16] = WriteDataSubwordDuplicated[23:16];
-      if (ByteMaskM[3]) FinalWriteDataM[31:24] = WriteDataSubwordDuplicated[31:24];
-    end 
-
   end
 endmodule
