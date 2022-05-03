4fbf78e049e36a883e1b85babd1dfbf09e816332
David Harris <david_harris@hmc.edu>
2022-05-03 08:31:54 -0700

    clean up sram1p1rw; still doesn't work on Modelsim 2022.1

--- setup.sh
+++ setup.sh
@@ -26,8 +26,9 @@ export PATH=$WALLY/bin:$PATH
 # Verilator
 export PATH=/usr/local/bin/verilator:$PATH # Change this for your path to Verilator
 # ModelSim/Questa (vsim)
-export PATH=/cad/mentor/questa_sim-2021.2_1/questasim/bin:$PATH    # Change this for your path to Modelsim, or delete
+# Note: 2022.1 complains on cache/sram1p1r1w about StoredData cannot be driven by multiple always_ff blocks.  Ues 2021.2 for now
 export PATH=/cad/mentor/questa_sim-2022.1_1/questasim/bin:$PATH    # Change this for your path to Modelsim 
+export PATH=/cad/mentor/questa_sim-2021.2_1/questasim/bin:$PATH    # Change this for your path to Modelsim, or delete
 export MGLS_LICENSE_FILE=1717@solidworks.eng.hmc.edu # Change this to your Siemens license server
 export PATH=/cad/synopsys/SYN/bin:$PATH  # Change this for your path to Design Compiler
 export SNPSLMD_LICENSE_FILE=27020@134.173.38.214
