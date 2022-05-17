357d77d3321950cd9abc860a0ceede6fc6c45f1c
slmnemo <nicholas.lucioforlife@yahoo.com>
2022-05-17 16:57:00 -0700

    Revert "Revert "Merge branch 'main' of github.com:davidharrishmc/riscv-wally into main""
    This reverts commit 0e3099743cf46391c4f45adfc0ce8afbbcbb1382.
    
    reverted the wrong commit

--- synthDC/scripts/synth.tcl
+++ synthDC/scripts/synth.tcl
@@ -137,6 +137,10 @@ if {$tech == "sky130"} {
 # Set the wire load model 
 set_wire_load_mode "top"
 
+# Set switching activities
+# default activity factors are 1 for clocks, 0.1 for others
+# static probability of 0.5 is used for leakage
+
 # Attempt Area Recovery - if looking for minimal area
 # set_max_area 2000
 
@@ -359,4 +363,4 @@ redirect $filename { report_constraint }
 set filename [format "%s%s%s%s" $outputDir  "/reports/" $my_toplevel "_hier.rep"]
 # redirect $filename { report_hierarchy }
 
-quit 
+#quit 
