84256924e768a3f48693508569f697d1c4c8e40b
Madeleine Masser-Frye <51804758+mmasserfrye@users.noreply.github.com>
2022-06-15 18:28:36 +0000

    cleanup, plots for paper

--- pipelined/src/ppa/ppa.sv
+++ pipelined/src/ppa/ppa.sv
@@ -549,6 +549,30 @@ module ppa_mux8d_1 #(parameter WIDTH = 1) (
   assign y = s[2] ? (s[1] ? (s[0] ? d5 : d4) : (s[0] ? d6 : d7)) : (s[1] ? (s[0] ? d3 : d2) : (s[0] ? d1 : d0)); 
 endmodule
 
+module ppa_mux2_1 #(parameter WIDTH = 1) (
+  input  logic [WIDTH-1:0] d0, d1, 
+  input  logic             s, 
+  output logic [WIDTH-1:0] y);
+
+  assign y = s ? d1 : d0; 
+endmodule
+
+module ppa_mux4_1 #(parameter WIDTH = 1) (
+  input  logic [WIDTH-1:0] d0, d1, d2, d3,
+  input  logic [1:0]       s, 
+  output logic [WIDTH-1:0] y);
+
+  assign y = s[1] ? (s[0] ? d3 : d2) : (s[0] ? d1 : d0); 
+endmodule
+
+module ppa_mux8_1 #(parameter WIDTH = 1) (
+  input  logic [WIDTH-1:0] d0, d1, d2, d3, d4, d5, d6, d7,
+  input  logic [2:0]       s, 
+  output logic [WIDTH-1:0] y);
+
+  assign y = s[2] ? (s[1] ? (s[0] ? d5 : d4) : (s[0] ? d6 : d7)) : (s[1] ? (s[0] ? d3 : d2) : (s[0] ? d1 : d0)); 
+endmodule
+
 module ppa_mux2_8 #(parameter WIDTH = 8) (
   input  logic [WIDTH-1:0] d0, d1, 
   input  logic             s, 
