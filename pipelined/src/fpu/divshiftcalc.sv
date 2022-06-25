6d6cc7bb489d35021b75f924efdd49949ff69e33
Katherine Parry <kparry4@gmail.com>
2022-06-25 00:04:53 +0000

    commented out error - also some divider bugs fixed

--- pipelined/src/fpu/divshiftcalc.sv
+++ pipelined/src/fpu/divshiftcalc.sv
@@ -10,12 +10,27 @@ module divshiftcalc(
     logic ResDenorm;
     logic [`NE+1:0] DenormShift;
     logic [`NE+1:0] NormShift;
-    assign ResDenorm = DivCalcExpM[`NE+1];
-    assign DenormShift = (`NE+2)'(`NF-1)+DivCalcExpM;
-    assign NormShift = {(`NE+1)'(0), ~Quot[`DIVLEN+2]} + (`NE+2)'(`NF);
+
+    // is the result denromalized
+    // if the exponent is 1 then the result needs to be normalized then the result is denormalizes
+    assign ResDenorm = DivCalcExpM[`NE+1]|(~|DivCalcExpM[`NE+1:1]&~(DivCalcExpM[0]&Quot[`DIVLEN+2]));
+    // if the result is denormalized
+    //  00000000x.xxxxxx...                     Exp = DivCalcExp
+    //  .00000000xxxxxxx... >> NF+1             Exp = DivCalcExp+NF+1
+    //  .000xxxxxxxxxxxx... << DivCalcExp+NF+1  Exp = 0
+    //  .0000xxxxxxxxxxx... >> 1                Exp = 1
+    // Left shift amount  = DivCalcExp+NF+1-1
+    assign DenormShift = (`NE+2)'(`NF)+DivCalcExpM;
+    // if the result is denormalized
+    //  00000000x.xxxxxx...                     Exp = DivCalcExp
+    //  .00000000xxxxxxx... >> NF+1             Exp = DivCalcExp+NF+1
+    //  00000000x.xxxxxx... << NF+1             Exp = DivCalcExp
+    //  00000000xx.xxxxx... << 1?               Exp = DivCalcExp-1
+    // Left shift amount  = NF+1 plus 1 if normalization required
+    assign NormShift = (`NE+2)'(`NF+1) + {(`NE+1)'(0), ~Quot[`DIVLEN+2]};
     assign DivShiftAmt = ResDenorm ?  DenormShift[$clog2(`NORMSHIFTSZ)-1:0] : NormShift[$clog2(`NORMSHIFTSZ)-1:0];
 
-    assign DivShiftIn = {(`NF)'(0), Quot[`DIVLEN+1:0], {`NORMSHIFTSZ-`DIVLEN-2-`NF{1'b0}}};
+    // assign DivShiftIn = {(`NF)'(0), Quot[`DIVLEN+2:0], {`NORMSHIFTSZ-`DIVLEN-3-`NF{1'b0}}};
     // the quotent is in the range [.5,2)
     // if the quotent < 1 and not denormal then subtract 1 to account for the normalization shift
     assign CorrDivExp = (ResDenorm&~DenormShift[`NE+1]) ? (`NE+2)'(0) : DivCalcExpM - {(`NE+1)'(0), ~Quot[`DIVLEN+2]};
