aa253748fc08992e1aa984d5d18bb4e36a73a48d
Madeleine Masser-Frye <51804758+mmasserfrye@users.noreply.github.com>
2022-06-28 02:28:13 +0000

    update wally synth analysis

--- synthDC/wallySynth.py
+++ synthDC/wallySynth.py
@@ -8,20 +8,22 @@ def runCommand(config, tech, freq):
     command = "make synth DESIGN=wallypipelinedcore CONFIG={} TECH={} DRIVE=FLOP FREQ={} MAXOPT=0 MAXCORES=1".format(config, tech, freq)
     subprocess.Popen(command, shell=True)
 
+testFreq = [3000, 10000]
+
 if __name__ == '__main__':
 
     techs = ['sky90', 'tsmc28']
-    bestAchieved = [750, 3000]
+    sweepCenter = [870, 3000]
     synthsToRun = []
 
-    
     arr = [-8, -6, -4, -2, 0, 2, 4, 6, 8]
     for i in [0, 1]:
         tech = techs[i]
-        f = bestAchieved[i]
-        for freq in [round(f+f*x/100) for x in arr]: # rv32e freq sweep
+        sc = sweepCenter[i]
+        f = testFreq[i]
+        for freq in [round(sc+sc*x/100) for x in arr]: # rv32e freq sweep
             synthsToRun += [['rv32e', tech, freq]]
-        for config in ['rv32gc', 'rv32ic', 'rv64gc', 'rv64i', 'rv64ic']: # configs
+        for config in ['rv32gc', 'rv32ic', 'rv64gc', 'rv64i', 'rv64ic', 'rv32e']: # configs
             synthsToRun += [[config, tech, f]]
         for mod in ['FPUoff', 'noMulDiv', 'noPriv', 'PMP0', 'PMP16']: # rv64gc path variations
             config = 'rv64gc_' + mod
