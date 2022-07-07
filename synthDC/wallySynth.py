3135e1202e999373d1046bbc8fc8ea446c1506af
Madeleine Masser-Frye <51804758+mmasserfrye@users.noreply.github.com>
2022-07-07 16:08:21 +0000

    plot tuning, fo4 axis

--- synthDC/wallySynth.py
+++ synthDC/wallySynth.py
@@ -9,7 +9,7 @@ def runCommand(config, tech, freq):
     commands = ["make fresh", "make synth DESIGN=wallypipelinedcore CONFIG={} TECH={} DRIVE=FLOP FREQ={} MAXOPT=0 MAXCORES=1".format(config, tech, freq)]
     for c in commands:
         subprocess.Popen(c, shell=True)
-    time.sleep(60)
+    # time.sleep(60) fix only do this when diff configs
 
 testFreq = [3000, 10000]
 
@@ -26,14 +26,14 @@ if __name__ == '__main__':
         tech = techs[i]
         sc = sweepCenter[i]
         f = testFreq[i]
-        # for freq in [round(sc+sc*x/100) for x in arr]: # rv32e freq sweep
-        #     synthsToRun += [['rv32e', tech, freq]]
+        for freq in [round(sc+sc*x/100) for x in arr]: # rv32e freq sweep
+            synthsToRun += [['rv32e', tech, freq]]
         # for config in ['rv32gc', 'rv32ic', 'rv64gc', 'rv64ic', 'rv32e']: # configs
         #     config = config + '_FPUoff' # while FPU under rennovation
         #     synthsToRun += [[config, tech, f]]
-        for mod in ['noMulDiv', 'noPriv', 'PMP0', 'PMP16']: # rv64gc path variations
-            config = 'rv64gc_' + mod
-            synthsToRun += [[config, tech, f]]
+        # for mod in ['noMulDiv', 'noPriv', 'PMP0', 'PMP16']: # rv64gc path variations
+        #     config = 'rv64gc_' + mod
+        #     synthsToRun += [[config, tech, f]]
 
         for x in synthsToRun:
             pool.starmap(runCommand, [x])
