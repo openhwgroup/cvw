8f0f6261407de38d8b94028cd091324899e3e866
Madeleine Masser-Frye <51804758+mmasserfrye@users.noreply.github.com>
2022-07-08 08:02:11 +0000

    made wally synth flow shell based

--- synthDC/wallySynth.py
+++ synthDC/wallySynth.py
@@ -4,36 +4,43 @@
 import subprocess
 from multiprocessing import Pool
 import time
+import sys
 
-def runCommand(config, tech, freq):
-    commands = ["make fresh", "make synth DESIGN=wallypipelinedcore CONFIG={} TECH={} DRIVE=FLOP FREQ={} MAXOPT=0 MAXCORES=1".format(config, tech, freq)]
-    for c in commands:
-        subprocess.Popen(c, shell=True)
-    # time.sleep(60) fix only do this when diff configs
+def runSynth(config, tech, freq):
+    global pool
+    command = "make synth DESIGN=wallypipelinedcore CONFIG={} TECH={} DRIVE=FLOP FREQ={} MAXOPT=1 MAXCORES=1".format(config, tech, freq)
+    pool.map(mask, [command])
+
+def mask(command):
+    subprocess.Popen(command, shell=True)
 
 testFreq = [3000, 10000]
 
 if __name__ == '__main__':
 
+    i = 0
     techs = ['sky90', 'tsmc28']
-    sweepCenter = [870, 2940]
     synthsToRun = []
-
+    tech = techs[i]
+    freq = testFreq[i]
     arr = [-8, -6, -4, -2, 0, 2, 4, 6, 8]
     pool = Pool()
+    staggerPeriod = 60 #seconds
 
-    for i in [0]: 
-        tech = techs[i]
-        sc = sweepCenter[i]
-        f = testFreq[i]
+    typeToRun = sys.argv[1]
+
+    if 'configs' in typeToRun:
+        for config in ['rv32gc', 'rv32ic', 'rv64gc', 'rv64ic', 'rv32e']: # configs
+            config = config + '_orig' # until memory integrated
+            runSynth(config, tech, freq)
+            time.sleep(staggerPeriod)
+    elif 'features' in typeToRun:
+        for mod in ['FPUoff', 'noMulDiv', 'noPriv', 'PMP0', 'PMP16']: # rv64gc path variations
+            config = 'rv64gc_' + mod
+            runSynth(config, tech, freq)
+            time.sleep(staggerPeriod)
+    elif 'freqs' in typeToRun:
+        sc = int(sys.argv[2])
+        config = 'rv32e'
         for freq in [round(sc+sc*x/100) for x in arr]: # rv32e freq sweep
-            synthsToRun += [['rv32e', tech, freq]]
-        # for config in ['rv32gc', 'rv32ic', 'rv64gc', 'rv64ic', 'rv32e']: # configs
-        #     config = config + '_FPUoff' # while FPU under rennovation
-        #     synthsToRun += [[config, tech, f]]
-        # for mod in ['noMulDiv', 'noPriv', 'PMP0', 'PMP16']: # rv64gc path variations
-        #     config = 'rv64gc_' + mod
-        #     synthsToRun += [[config, tech, f]]
-
-        for x in synthsToRun:
-            pool.starmap(runCommand, [x])
+            runSynth(config, tech, freq)
