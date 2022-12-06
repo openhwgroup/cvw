055ca9ee37145b4180bf3fca341a5b8c09fa8ca9
Kip Macsai-Goren <kmacsaigoren@hmc.edu>
2022-12-06 09:56:08 -0800

    Addded fix for 32 bit periph test and added test to regression

--- pipelined/src/ifu/ifu.sv
+++ pipelined/src/ifu/ifu.sv
@@ -264,6 +264,7 @@ module ifu (
 
       if(`IROM_SUPPORTED) mux2 #(32) UnCachedDataMux2(FetchBuffer, IROMInstrF, SelIROM, InstrRawF);
       else assign InstrRawF = FetchBuffer;
+      assign CacheCommittedF = 1'b0;
       assign IFUHBURST = 3'b0;
       assign {ICacheFetchLine, ICacheStallF, FinalInstrRawF} = '0;
       assign {ICacheMiss, ICacheAccess} = '0;
