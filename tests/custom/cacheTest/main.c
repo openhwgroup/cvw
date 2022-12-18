e8c1d14abbe2b4f9d24c441478108bd0a891aaf1
Ross Thompson <ross1728@gmail.com>
2022-12-18 17:20:30 -0600

    Have a basic cache test to fill all ways and sets.

--- tests/custom/cacheTest/main.c
+++ tests/custom/cacheTest/main.c
@@ -1,10 +1,11 @@
 #include "header.h"
 
-int main(){
+int main(int argc, char *argv[]){
   long int array [1024];
   int index;
   for(index = 0; index < 1024; index++) {
     array[index] = index;
   }
-  return 0;
+  *argv = array;
+  return array[1023] + argc;
 }
