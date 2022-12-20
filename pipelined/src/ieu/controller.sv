8f640f050f8ef517c054173c0d23942b993c5004
David Harris <david_harris@hmc.edu>
2022-12-20 15:38:30 -0800

    IFU mux for CSRWriteFenceM conditional on ZICSR/ZIFENCEI

--- pipelined/src/ieu/controller.sv
+++ pipelined/src/ieu/controller.sv
@@ -130,7 +130,7 @@ module controller(
       7'b0001111: if(`ZIFENCEI_SUPPORTED)
                     ControlsD = `CTRLW'b0_000_00_00_000_0_0_0_0_0_0_0_1_0_00_0; // fence
               	  else
-                    ControlsD = `CTRLW'b0_000_00_00_000_0_0_0_0_0_0_0_0_0_00_0; // fence
+                    ControlsD = `CTRLW'b0_000_00_00_000_0_0_0_0_0_0_0_0_0_00_0; // fence treated as nop
       7'b0010011:   ControlsD = `CTRLW'b1_000_01_00_000_0_1_0_0_0_0_0_0_0_00_0; // I-type ALU
       7'b0010111:   ControlsD = `CTRLW'b1_100_11_00_000_0_0_0_0_0_0_0_0_0_00_0; // auipc
       7'b0011011: if (`XLEN == 64)
