47c46bc9b5f9c18f745911aeb7f9800b54d605c4
Jacob Pease <jacobpease@protonmail.com>
2023-01-13 14:36:23 -0600

    Added IPs to wally.tcl.

--- fpga/generator/wally.tcl
+++ fpga/generator/wally.tcl
@@ -16,6 +16,9 @@ read_ip IP/xlnx_ddr4.srcs/sources_1/ip/xlnx_ddr4/xlnx_ddr4.xci
 
 # Added crossbar - Jacob Pease <2023-01-12 Thu>
 read_ip IP/xlnx_axi_crossbar.srcs/sources_1/ip/xlnx_axi_crossbar/xlnx_axi_crossbar.xci
+read_ip IP/xlnx_axi_dwidth_conv_32to64.srcs/sources_1/ip/xlnx_axi_dwidth_conv_32to64/xlnx_axi_dwidth_conv_32to64.xci
+read_ip IP/xlnx_axi_dwidth_conv_64to32.srcs/sources_1/ip/xlnx_axi_dwidth_conv_64to32/xlnx_axi_dwidth_conv_64to32.xci
+
 
 
 read_verilog -sv [glob -type f ../../pipelined/src/*/*.sv ../../pipelined/src/*/*/*.sv]
