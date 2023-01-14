53d0d2882833f0b53af2149a5a4729b4f2fcddc9
David Harris <david_harris@hmc.edu>
2023-01-13 22:12:06 -0800

    csr cleanup

--- pipelined/src/privileged/csrs.sv
+++ pipelined/src/privileged/csrs.sv
@@ -41,11 +41,7 @@ module csrs #(parameter
   SCAUSE = 12'h142,
   STVAL = 12'h143,
   SIP= 12'h144,
-  SATP = 12'h180,
-    // Constants
-   ZERO = {(`XLEN){1'b0}},
-   SEDELEG_MASK = ~(ZERO | `XLEN'b111 << 9)
-  ) (
+  SATP = 12'h180) (
     input logic 	     clk, reset, 
     input logic 	     InstrValidNotFlushedM, 
     input logic 	     CSRSWriteM, STrapM,
@@ -63,67 +59,60 @@ module csrs #(parameter
     output logic 	     IllegalCSRSAccessM
 );
 
+  // Constants
+  localparam ZERO = {(`XLEN){1'b0}};
+  localparam SEDELEG_MASK = ~(ZERO | `XLEN'b111 << 9);
 
-  // Supervisor mode CSRs sometimes supported
-  if (`S_SUPPORTED) begin:csrs
-    logic WriteSTVECM;
-    logic WriteSSCRATCHM, WriteSEPCM;
-    logic WriteSCAUSEM, WriteSTVALM, WriteSATPM, WriteSCOUNTERENM;
-    (* mark_debug = "true" *)    logic [`XLEN-1:0] SSCRATCH_REGW, STVAL_REGW;
-    (* mark_debug = "true" *) logic [`XLEN-1:0] SCAUSE_REGW;      
-    
-    assign WriteSSTATUSM = CSRSWriteM & (CSRAdrM == SSTATUS)  & InstrValidNotFlushedM;
-    assign WriteSTVECM = CSRSWriteM & (CSRAdrM == STVEC) & InstrValidNotFlushedM;
-    assign WriteSSCRATCHM = CSRSWriteM & (CSRAdrM == SSCRATCH) & InstrValidNotFlushedM;
-    assign WriteSEPCM = STrapM | (CSRSWriteM & (CSRAdrM == SEPC)) & InstrValidNotFlushedM;
-    assign WriteSCAUSEM = STrapM | (CSRSWriteM & (CSRAdrM == SCAUSE)) & InstrValidNotFlushedM;
-    assign WriteSTVALM = STrapM | (CSRSWriteM & (CSRAdrM == STVAL)) & InstrValidNotFlushedM;
-    assign WriteSATPM = CSRSWriteM & (CSRAdrM == SATP) & (PrivilegeModeW == `M_MODE | ~STATUS_TVM) & InstrValidNotFlushedM;
-    assign WriteSCOUNTERENM = CSRSWriteM & (CSRAdrM == SCOUNTEREN) & InstrValidNotFlushedM;
+  logic               WriteSTVECM;
+  logic               WriteSSCRATCHM, WriteSEPCM;
+  logic               WriteSCAUSEM, WriteSTVALM, WriteSATPM, WriteSCOUNTERENM;
+  (* mark_debug = "true" *)  logic [`XLEN-1:0] SSCRATCH_REGW, STVAL_REGW;
+  (* mark_debug = "true" *) logic [`XLEN-1:0] SCAUSE_REGW;      
+  
+  // write enables
+  assign WriteSSTATUSM = CSRSWriteM & (CSRAdrM == SSTATUS)  & InstrValidNotFlushedM;
+  assign WriteSTVECM = CSRSWriteM & (CSRAdrM == STVEC) & InstrValidNotFlushedM;
+  assign WriteSSCRATCHM = CSRSWriteM & (CSRAdrM == SSCRATCH) & InstrValidNotFlushedM;
+  assign WriteSEPCM = STrapM | (CSRSWriteM & (CSRAdrM == SEPC)) & InstrValidNotFlushedM;
+  assign WriteSCAUSEM = STrapM | (CSRSWriteM & (CSRAdrM == SCAUSE)) & InstrValidNotFlushedM;
+  assign WriteSTVALM = STrapM | (CSRSWriteM & (CSRAdrM == STVAL)) & InstrValidNotFlushedM;
+  assign WriteSATPM = CSRSWriteM & (CSRAdrM == SATP) & (PrivilegeModeW == `M_MODE | ~STATUS_TVM) & InstrValidNotFlushedM;
+  assign WriteSCOUNTERENM = CSRSWriteM & (CSRAdrM == SCOUNTEREN) & InstrValidNotFlushedM;
 
-    // CSRs
-    flopenr #(`XLEN) STVECreg(clk, reset, WriteSTVECM, {CSRWriteValM[`XLEN-1:2], 1'b0, CSRWriteValM[0]}, STVEC_REGW); 
-    flopenr #(`XLEN) SSCRATCHreg(clk, reset, WriteSSCRATCHM, CSRWriteValM, SSCRATCH_REGW);
-    flopenr #(`XLEN) SEPCreg(clk, reset, WriteSEPCM, NextEPCM, SEPC_REGW); 
-    flopenr #(`XLEN) SCAUSEreg(clk, reset, WriteSCAUSEM, NextCauseM, SCAUSE_REGW);
-    flopenr #(`XLEN) STVALreg(clk, reset, WriteSTVALM, NextMtvalM, STVAL_REGW);
-    if (`VIRTMEM_SUPPORTED)
-      flopenr #(`XLEN) SATPreg(clk, reset, WriteSATPM, CSRWriteValM, SATP_REGW);
-    else
-      assign SATP_REGW = 0; // hardwire to zero if virtual memory not supported
-    flopens #(32)   SCOUNTERENreg(clk, reset, WriteSCOUNTERENM, CSRWriteValM[31:0], SCOUNTEREN_REGW);
+  // CSRs
+  flopenr #(`XLEN) STVECreg(clk, reset, WriteSTVECM, {CSRWriteValM[`XLEN-1:2], 1'b0, CSRWriteValM[0]}, STVEC_REGW); 
+  flopenr #(`XLEN) SSCRATCHreg(clk, reset, WriteSSCRATCHM, CSRWriteValM, SSCRATCH_REGW);
+  flopenr #(`XLEN) SEPCreg(clk, reset, WriteSEPCM, NextEPCM, SEPC_REGW); 
+  flopenr #(`XLEN) SCAUSEreg(clk, reset, WriteSCAUSEM, NextCauseM, SCAUSE_REGW);
+  flopenr #(`XLEN) STVALreg(clk, reset, WriteSTVALM, NextMtvalM, STVAL_REGW);
+  if (`VIRTMEM_SUPPORTED)
+    flopenr #(`XLEN) SATPreg(clk, reset, WriteSATPM, CSRWriteValM, SATP_REGW);
+  else
+    assign SATP_REGW = 0; // hardwire to zero if virtual memory not supported
+  flopens #(32)   SCOUNTERENreg(clk, reset, WriteSCOUNTERENM, CSRWriteValM[31:0], SCOUNTEREN_REGW);
 
-    // CSR Reads
-    always_comb begin:csrr
-      IllegalCSRSAccessM = 0;
-      case (CSRAdrM) 
-        SSTATUS:   CSRSReadValM = SSTATUS_REGW;
-        STVEC:     CSRSReadValM = STVEC_REGW;
-        SIP:       CSRSReadValM = {{(`XLEN-12){1'b0}}, MIP_REGW & 12'h222 & MIDELEG_REGW}; // only read supervisor fields  
-        SIE:       CSRSReadValM = {{(`XLEN-12){1'b0}}, MIE_REGW & 12'h222}; // only read supervisor fields
-        SSCRATCH:  CSRSReadValM = SSCRATCH_REGW;
-        SEPC:      CSRSReadValM = SEPC_REGW;
-        SCAUSE:    CSRSReadValM = SCAUSE_REGW;
-        STVAL:     CSRSReadValM = STVAL_REGW;
-        SATP:      if (`VIRTMEM_SUPPORTED & (PrivilegeModeW == `M_MODE | ~STATUS_TVM)) CSRSReadValM = SATP_REGW;
-                    else begin
-                      CSRSReadValM = 0;
-                      if (PrivilegeModeW == `S_MODE & STATUS_TVM) IllegalCSRSAccessM = 1;
-                    end
-        SCOUNTEREN:CSRSReadValM = {{(`XLEN-32){1'b0}}, SCOUNTEREN_REGW};
-        default: begin
-                    CSRSReadValM = 0; 
-                    IllegalCSRSAccessM = 1;  
-        end       
-      endcase
-    end
-  end else begin
-    assign WriteSSTATUSM = 0;
-    assign CSRSReadValM = 0;
-    assign SEPC_REGW = 0;
-    assign STVEC_REGW = 0;
-    assign SCOUNTEREN_REGW = 0;
-    assign SATP_REGW = 0;
-    assign IllegalCSRSAccessM = 1;
+  // CSR Reads
+  always_comb begin:csrr
+    IllegalCSRSAccessM = 0;
+    case (CSRAdrM) 
+      SSTATUS:   CSRSReadValM = SSTATUS_REGW;
+      STVEC:     CSRSReadValM = STVEC_REGW;
+      SIP:       CSRSReadValM = {{(`XLEN-12){1'b0}}, MIP_REGW & 12'h222 & MIDELEG_REGW}; // only read supervisor fields  
+      SIE:       CSRSReadValM = {{(`XLEN-12){1'b0}}, MIE_REGW & 12'h222}; // only read supervisor fields
+      SSCRATCH:  CSRSReadValM = SSCRATCH_REGW;
+      SEPC:      CSRSReadValM = SEPC_REGW;
+      SCAUSE:    CSRSReadValM = SCAUSE_REGW;
+      STVAL:     CSRSReadValM = STVAL_REGW;
+      SATP:      if (`VIRTMEM_SUPPORTED & (PrivilegeModeW == `M_MODE | ~STATUS_TVM)) CSRSReadValM = SATP_REGW;
+                  else begin
+                    CSRSReadValM = 0;
+                    if (PrivilegeModeW == `S_MODE & STATUS_TVM) IllegalCSRSAccessM = 1;
+                  end
+      SCOUNTEREN:CSRSReadValM = {{(`XLEN-32){1'b0}}, SCOUNTEREN_REGW};
+      default: begin
+                  CSRSReadValM = 0; 
+                  IllegalCSRSAccessM = 1;  
+      end       
+    endcase
   end
 endmodule
