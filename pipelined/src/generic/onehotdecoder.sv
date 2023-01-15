fa61c9c4a5210f89e4409b0f046b3ce33ed3a26b
David Harris <david_harris@hmc.edu>
2023-01-14 18:56:46 -0800

    generic cleanup

--- pipelined/src/generic/onehotdecoder.sv
+++ pipelined/src/generic/onehotdecoder.sv
@@ -26,11 +26,10 @@
 
 `include "wally-config.vh"
 
-module onehotdecoder
-  #(parameter WIDTH = 2)
-  (input logic [WIDTH-1:0] bin,
-   output logic [2**WIDTH-1:0] decoded
-   );
+module onehotdecoder #(parameter WIDTH = 2) (
+  input  logic [WIDTH-1:0]    bin,
+  output logic [2**WIDTH-1:0] decoded
+);
 
   always_comb begin
     decoded = '0;
