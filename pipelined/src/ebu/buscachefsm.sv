cd5e62119a7ec878e462bbf4cf46238a38e03426
Ross Thompson <ross1728@gmail.com>
2023-01-17 22:22:23 -0600

    Added commenets and formating to abhcachefsm and abhcacheinterface.

--- pipelined/src/ebu/buscachefsm.sv
+++ pipelined/src/ebu/buscachefsm.sv
@@ -33,26 +33,29 @@ module buscachefsm #(parameter integer BeatCountThreshold, LOGWPL) (
   input  logic              HRESETn,
 
   // IEU interface
-  input  logic              Flush,
-  input  logic [1:0]        BusRW,
-  input  logic              Stall,
-  output logic              BusCommitted,
-  output logic              BusStall,
-  output logic              CaptureEn,
+  input  logic              Stall,              // Core pipeline is stalled
+  input  logic              Flush,              // Pipeline stage flush. Prevents bus transaction from starting
+  input  logic [1:0]        BusRW,              // Uncached memory operation read/write control: 10: read, 01: write
+  output logic              BusStall,           // Bus is busy with an in flight memory operation
+  output logic              BusCommitted,       // Bus is busy with an in flight memory operation and it is not safe to take an interrupt
+
+  // ahb cache interface locals.
+  output logic              CaptureEn,          // Enable updating the Fetch buffer with valid data from HRDATA
 
   // cache interface
-  input  logic [1:0]        CacheBusRW,
-  output logic              CacheBusAck,
+  input  logic [1:0]        CacheBusRW,         // Cache bus operation, 01: writeback, 10: fetch
+  output logic              CacheBusAck,        // Handshack to $ indicating bus transaction completed
   
   // lsu interface
-  output logic [LOGWPL-1:0] BeatCount, BeatCountDelayed,
-  output logic              SelBusBeat,
+  output logic [LOGWPL-1:0] BeatCount,          // Beat position within the cache line in the Address Phase
+  output logic [LOGWPL-1:0] BeatCountDelayed,   // Beat within the cache line in the second (Data) cache stage
+  output logic              SelBusBeat,         // Tells the cache to select the word from ReadData or WriteData from BeatCount rather than PAdr
 
   // BUS interface
-  input  logic              HREADY,
-  output logic [1:0]        HTRANS,
-  output logic              HWRITE,
-  output logic [2:0]        HBURST
+  input  logic              HREADY,             // AHB peripheral ready
+  output logic [1:0]        HTRANS,             // AHB transaction type, 00: IDLE, 10 NON_SEQ, 11 SEQ
+  output logic              HWRITE,             // AHB 0: Read operation 1: Write operation 
+  output logic [2:0]        HBURST              // AHB burst length
 );
   
   typedef enum logic [2:0] {ADR_PHASE, DATA_PHASE, MEM3, CACHE_FETCH, CACHE_WRITEBACK}               busstatetype;
