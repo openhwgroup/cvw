442de3f5b7022c40a4661af7582fba38429291a9
Ross Thompson <ross1728@gmail.com>
2023-01-20 20:16:33 -0600

    Updated fpga constraints.

--- fpga/generator/insert_debug_comment.sh
+++ fpga/generator/insert_debug_comment.sh
@@ -26,14 +26,13 @@
 
 # This script copies wally's pipelined#src to fpga#src#CopiedFiles_do_not_add_to_repo
 # Then it processes them to add mark_debug on signals needed by the FPGA's ILA.
-copiedDir="..#src#CopiedFiles_do_not_add_to_repo"
+copiedDir="../src/CopiedFiles_do_not_add_to_repo"
 while read line; do
     readarray -d ":" -t StrArray <<< "$line"
-    file="${copiedDir}#${StrArray[0]}"
-    #signal=`echo "${StrArray[1]}" | awk '{$1=$1};1'`
+    file="${copiedDir}/${StrArray[0]}"
     signal=`echo "${StrArray[1]}" | awk '{$1=$1};1'`
     readarray -d " " -t SigArray <<< $signal
     sigType=`echo "${SigArray[0]}" | awk '{$1=$1};1'`
     sigName=`echo "${SigArray[1]}" | awk '{$1=$1};1'`
-    find $copiedDir -wholename $file | xargs sed -i "s#\(.*${sigType}.*${sigName}\)#(\* mark_debug = \"true\" \*)\1#g" 
-done < ..#constraints#marked_debug.txt
+    find $copiedDir -wholename $file | xargs sed -i "s/\(.*${sigType}.*${sigName}\)/(\* mark_debug = \"true\" \*)\1/g" 
+done < ../constraints/marked_debug.txt
