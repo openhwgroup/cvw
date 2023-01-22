f7c89abe06b9e1cfb6ffc8eee476c3b6c7fb7a75
Ross Thompson <ross1728@gmail.com>
2023-01-22 12:53:23 -0600

    Added SPDX header to install script.

--- bin/wally-tool-chain-install.sh
+++ bin/wally-tool-chain-install.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
+###########################################
+## cache 
+##
+## Written: Ross Thompson ross1728@gmail.com
+## Created: 18 January 2023
+## Modified: 22 January 2023
+##
+## Purpose: Open source tool chain installation script
+##
+## A component of the CORE-V-WALLY configurable RISC-V project.
+##
+## Copyright (C) 2021-23 Harvey Mudd College & Oklahoma State University
+##
+## SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+##
+## Licensed under the Solderpad Hardware License v 2.1 (the “License”); you may not use this file 
+## except in compliance with the License, or, at your option, the Apache License version 2.0. You 
+## may obtain a copy of the License at
+##
+## https:##solderpad.org/licenses/SHL-2.1/
+##
+## Unless required by applicable law or agreed to in writing, any work distributed under the 
+## License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+## either express or implied. See the License for the specific language governing permissions 
+## and limitations under the License.
+################################################################################################
 
 export RISCV="${1:-/opt/riscv}"
 export PATH=$PATH:$RISCV/bin
