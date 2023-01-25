5030a56f57a8473f396b690aad21c93b0829da82
Ross Thompson <ross1728@gmail.com>
2023-01-25 11:41:16 -0600

    Optomized gshare.

--- pipelined/src/ifu/brpred/speculativegshare.sv
+++ pipelined/src/ifu/brpred/speculativegshare.sv
@@ -45,7 +45,7 @@ module speculativegshare
    input logic             PCSrcE
    );
 
-  logic                    MatchF, MatchD, MatchE, MatchM;
+  logic                    MatchF, MatchD, MatchE;
   logic                    MatchNextX, MatchXF;
 
   logic [1:0]              TableDirPredictionF, DirPredictionD, DirPredictionE;
@@ -57,7 +57,7 @@ module speculativegshare
   logic [k:-1] 			   GHRNextD, OldGHRD;
   logic [k:0]              GHRNextE, GHRNextM, GHRNextW;
   logic [k-1:0]            IndexNextF, IndexF;
-  logic [k-1:0]            IndexD, IndexE, IndexM;
+  logic [k-1:0]            IndexD, IndexE;
   
   logic                    PCSrcM, PCSrcW;
   logic [`XLEN-1:0]        PCW;
@@ -68,15 +68,14 @@ module speculativegshare
   assign IndexF = GHRF        ^ {PCF[k+1] ^ PCF[1], PCF[k:2]};
   assign IndexD = GHRD[k-1:0] ^ {PCD[k+1] ^ PCD[1], PCD[k:2]};
   assign IndexE = GHRE[k-1:0] ^ {PCE[k+1] ^ PCE[1], PCE[k:2]};
-  assign IndexM = GHRM[k-1:0] ^ {PCM[k+1] ^ PCM[1], PCM[k:2]};
       
   ram2p1r1wbe #(2**k, 2) PHT(.clk(clk),
     .ce1(~StallF | reset), .ce2(~StallW & ~FlushW),
     .ra1(IndexNextF),
     .rd1(TableDirPredictionF),
-    .wa2(IndexM),
-    .wd2(NewDirPredictionM),
-    .we2(BranchInstrM & ~StallW & ~FlushW),
+    .wa2(IndexE),
+    .wd2(NewDirPredictionE),
+    .we2(BranchInstrE & ~StallM & ~FlushM),
     .bwe2(1'b1));
 
   // if there are non-flushed branches in the pipeline we need to forward the prediction from that stage to the NextF demi stage
@@ -84,15 +83,13 @@ module speculativegshare
   assign MatchF = BranchInstrF & ~FlushD & (IndexNextF == IndexF);
   assign MatchD = BranchInstrD & ~FlushE & (IndexNextF == IndexD);
   assign MatchE = BranchInstrE & ~FlushM & (IndexNextF == IndexE);
-  assign MatchM = BranchInstrM & ~FlushW & (IndexNextF == IndexM);
-  assign MatchNextX = MatchF | MatchD | MatchE | MatchM;
+  assign MatchNextX = MatchF | MatchD | MatchE;
 
   flopenr #(1) MatchReg(clk, reset, ~StallF, MatchNextX, MatchXF);
 
   assign ForwardNewDirPrediction = MatchF ? NewDirPredictionF :
                                    MatchD ? NewDirPredictionD :
-                                   MatchE ? NewDirPredictionE :
-                                   NewDirPredictionM;
+                                   NewDirPredictionE ;
   
   flopenr #(2) ForwardDirPredicitonReg(clk, reset, ~StallF, ForwardNewDirPrediction, ForwardDirPredictionF);
 
