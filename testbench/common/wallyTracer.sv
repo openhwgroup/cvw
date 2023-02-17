0da32a41f69c4c20d17bb272f97e52b181a36318
David Harris <david_harris@hmc.edu>
2023-02-16 19:40:27 -0800

    moved riscvassertons to its own file, added proper license headers to testbench support files

--- testbench/common/wallyTracer.sv
+++ testbench/common/wallyTracer.sv
@@ -1,3 +1,24 @@
+///////////////////////////////////////////
+// wallyTracer.sv
+//
+// A component of the Wally configurable RISC-V project.
+// 
+// Copyright (C) 2021 Harvey Mudd College & Oklahoma State University
+//
+// SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+//
+// Licensed under the Solderpad Hardware License v 2.1 (the “License”); you may not use this file 
+// except in compliance with the License, or, at your option, the Apache License version 2.0. You 
+// may obtain a copy of the License at
+//
+// https://solderpad.org/licenses/SHL-2.1/
+//
+// Unless required by applicable law or agreed to in writing, any work distributed under the 
+// License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+// either express or implied. See the License for the specific language governing permissions 
+// and limitations under the License.
+////////////////////////////////////////////////////////////////////////////////////////////////
+
 `include "wally-config.vh"
 
 `define NUM_REGS 32
@@ -14,7 +35,12 @@ module wallyTracer(rvviTrace rvvi);
   
   // wally specific signals
   logic 						 reset;
-  
+
+  logic 						 clk;
+  logic                          InstrValidD, InstrValidE;
+  logic                          StallF, StallD;
+  logic                          STATUS_SXL, STATUS_UXL;
+
   logic [`XLEN-1:0] 			 PCNextF, PCF, PCD, PCE, PCM, PCW;
   logic [`XLEN-1:0] 			 InstrRawD, InstrRawE, InstrRawM, InstrRawW;
   logic 						 InstrValidM, InstrValidW;
