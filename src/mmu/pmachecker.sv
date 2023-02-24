60752fe51c1dd1460977834477c73a4735ccca57
David Harris <david_harris@hmc.edu>
2023-02-24 07:52:46 -0800

    Fixed special cases of address decoder and documented better

--- src/mmu/pmachecker.sv
+++ src/mmu/pmachecker.sv
@@ -58,8 +58,12 @@ module pmachecker (
 
   // Only non-core RAM/ROM memory regions are cacheable
   assign Cacheable = SelRegions[8] | SelRegions[7] | SelRegions[6];
-  assign Idempotent = SelRegions[10] | SelRegions[9] | SelRegions[8] | SelRegions[6];
-  assign AtomicAllowed = SelRegions[10] | SelRegions[9] | SelRegions[8] | SelRegions[6];
+  // Nonidemdempotent means access could have side effect and must not be done speculatively or redundantly
+  // I/O is nonidempotent.  
+  assign Idempotent = SelRegions[10] | SelRegions[9] | SelRegions[8] | SelRegions[7] | SelRegions[6];
+  // Atomic operations are only allowed on RAM
+  assign AtomicAllowed = SelRegions[10] | SelRegions[8] | SelRegions[6];
+  // Check if tightly integrated memories are selected
   assign SelTIM = SelRegions[10] | SelRegions[9];
 
   // Detect access faults
