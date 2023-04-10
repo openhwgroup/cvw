c4e5b8db49262efe0da04a1e52744ed322cb1576
Ross Thompson <ross1728@gmail.com>
2023-04-10 17:02:19 -0500

    Updates for arty a7.

--- fpga/generator/wally.tcl
+++ fpga/generator/wally.tcl
@@ -73,7 +73,7 @@ report_clock_interaction                                                -file re
 
 write_verilog -force -mode funcsim sim/syn-funcsim.v
 
-if {$board=="ArtyA7"}{
+if {$board=="ArtyA7"} {
     source ../constraints/small-debug.xdc
 
 } else {    
