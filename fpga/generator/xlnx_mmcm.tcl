4ad33d7acce1fcdc8659f00a3bd1a26b356e7685
Ross Thompson <ross1728@gmail.com>
2023-04-17 11:10:19 -0500

    OMG. the ddr3 has it's own mmcm (pll) which had incorreclty specified the input clock period as 3000 ps rather than 6000 ps so the pll was running at twice the speed. I speed the whole weekend on this. :(

--- fpga/generator/xlnx_mmcm.tcl
+++ fpga/generator/xlnx_mmcm.tcl
@@ -13,7 +13,7 @@ set_property -dict [list CONFIG.PRIM_IN_FREQ {100.000} \
                         CONFIG.CLKOUT2_USED {true} \
                         CONFIG.CLKOUT3_USED {true} \
                         CONFIG.CLKOUT4_USED {false} \
-                        CONFIG.CLKOUT1_REQUESTED_OUT_FREQ {166.66667} \
+                        CONFIG.CLKOUT1_REQUESTED_OUT_FREQ {166.6667} \
                         CONFIG.CLKOUT2_REQUESTED_OUT_FREQ {200} \
                         CONFIG.CLKOUT3_REQUESTED_OUT_FREQ {20} \
                         CONFIG.CLKIN1_JITTER_PS {10.0} \
