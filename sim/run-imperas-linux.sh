e72fa0c081d3067d4f321d7f2c2bfb9a2f78d66f
Ross Thompson <ross1728@gmail.com>
2023-04-26 17:29:57 -0500

    Modified the imperas linux scripts so they run without reporting hundreds of gigabytes of data.

--- sim/run-imperas-linux.sh
+++ sim/run-imperas-linux.sh
@@ -4,6 +4,7 @@
 
 export IMPERAS_TOOLS=$(pwd)/imperas.ic
 export OTHERFLAGS="+TRACE2LOG_ENABLE=1"
-export OTHERFLAGS="+TRACE2LOG_ENABLE=1 +TRACE2LOG_AFTER=10500000" 
+#export OTHERFLAGS="+TRACE2LOG_ENABLE=1  +TRACE2LOG_AFTER=10500000"
+export OTHERFLAGS=""
 
 vsim -c -do "do wally-linux-imperas.do buildroot buildroot-no-trace $::env(RISCV) 0 0 0"
