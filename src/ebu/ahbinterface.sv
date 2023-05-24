1299319d0b3008372bb0c21de4ff15615360838b
Ross Thompson <ross1728@gmail.com>
2023-05-24 14:56:02 -0500

    More parameterization.  Based on Lim's work.  EBU, IFU (except bpred), and IEU done.

--- src/ebu/ahbinterface.sv
+++ src/ebu/ahbinterface.sv
@@ -27,8 +27,6 @@
 // and limitations under the License.
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
-`include "wally-config.vh"
-
 module ahbinterface #(
   parameter XLEN,
   parameter LSU = 0                                   // 1: LSU bus width is `XLEN, 0: IFU bus width is 32 bits
@@ -38,30 +36,30 @@ module ahbinterface #(
   input  logic                          HREADY,       // AHB peripheral ready
   output logic [1:0]                    HTRANS,       // AHB transaction type, 00: IDLE, 10 NON_SEQ, 11 SEQ
   output logic                          HWRITE,       // AHB 0: Read operation 1: Write operation 
-  input  logic [`XLEN-1:0]              HRDATA,       // AHB read data
-  output logic [`XLEN-1:0]              HWDATA,       // AHB write data
-  output logic [`XLEN/8-1:0]            HWSTRB,       // AHB byte mask
+  input  logic [XLEN-1:0]              HRDATA,       // AHB read data
+  output logic [XLEN-1:0]              HWDATA,       // AHB write data
+  output logic [XLEN/8-1:0]            HWSTRB,       // AHB byte mask
   
   // lsu/ifu interface
   input  logic                          Stall,        // Core pipeline is stalled
   input  logic                          Flush,        // Pipeline stage flush. Prevents bus transaction from starting
   input  logic [1:0]                    BusRW,        // Memory operation read/write control: 10: read, 01: write
-  input  logic [`XLEN/8-1:0]            ByteMask,     // Bytes enables within a word
-  input  logic [`XLEN-1:0]              WriteData,    // IEU write data for a store
+  input  logic [XLEN/8-1:0]            ByteMask,     // Bytes enables within a word
+  input  logic [XLEN-1:0]              WriteData,    // IEU write data for a store
   output logic                          BusStall,     // Bus is busy with an in flight memory operation
   output logic                          BusCommitted, // Bus is busy with an in flight memory operation and it is not safe to take an interrupt
-  output logic [(LSU ? `XLEN : 32)-1:0] FetchBuffer   // Register to hold HRDATA after arriving from the bus
+  output logic [(LSU ? XLEN : 32)-1:0] FetchBuffer   // Register to hold HRDATA after arriving from the bus
 );
   
   logic                                 CaptureEn;
-  localparam                            LEN = (LSU ? `XLEN : 32);   // 32 bits for IFU, XLEN for LSU
+  localparam                            LEN = (LSU ? XLEN : 32);   // 32 bits for IFU, XLEN for LSU
   
   flopen #(LEN) fb(.clk(HCLK), .en(CaptureEn), .d(HRDATA[LEN-1:0]), .q(FetchBuffer));
 
   if(LSU) begin
     // delay HWDATA by 1 cycle per spec; assumes AHBW = XLEN    
-    flop #(`XLEN)   wdreg(HCLK, WriteData, HWDATA); 
-    flop #(`XLEN/8) HWSTRBReg(HCLK, ByteMask, HWSTRB);
+    flop #(XLEN)   wdreg(HCLK, WriteData, HWDATA); 
+    flop #(XLEN/8) HWSTRBReg(HCLK, ByteMask, HWSTRB);
   end else begin
     assign HWDATA = '0;
     assign HWSTRB = '0;
