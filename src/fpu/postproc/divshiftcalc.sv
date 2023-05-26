e6d25b7f70828507023bca24d8c36ead0cdfda4d
Ross Thompson <ross1728@gmail.com>
2023-05-26 14:40:06 -0500

    Finished fpu parameterization using Lim's method.

--- src/fpu/postproc/divshiftcalc.sv
+++ src/fpu/postproc/divshiftcalc.sv
@@ -26,24 +26,22 @@
 // and limitations under the License.
 ////////////////////////////////////////////////////////////////////////////////////////////////`include "wally-config.vh"
 
-`include "wally-config.vh"
-
-module divshiftcalc(
-  input  logic [`DIVb:0]              DivQm,              // divsqrt significand
-  input  logic [`NE+1:0]              DivQe,              // divsqrt exponent
-  output logic [`LOGNORMSHIFTSZ-1:0]  DivShiftAmt,        // divsqrt shift amount
-  output logic [`NORMSHIFTSZ-1:0]     DivShiftIn,         // divsqrt shift input
+module divshiftcalc import cvw::*;  #(parameter cvw_t P) (
+  input  logic [P.DIVb:0]              DivQm,              // divsqrt significand
+  input  logic [P.NE+1:0]              DivQe,              // divsqrt exponent
+  output logic [P.LOGNORMSHIFTSZ-1:0]  DivShiftAmt,        // divsqrt shift amount
+  output logic [P.NORMSHIFTSZ-1:0]     DivShiftIn,         // divsqrt shift input
   output logic                        DivResSubnorm,      // is the divsqrt result subnormal
   output logic                        DivSubnormShiftPos  // is the subnormal shift amount positive
 );
 
-  logic [`LOGNORMSHIFTSZ-1:0]         NormShift;          // normalized result shift amount
-  logic [`LOGNORMSHIFTSZ-1:0]         DivSubnormShiftAmt; // subnormal result shift amount (killed if negitive)
-  logic [`NE+1:0]                     DivSubnormShift;    // subnormal result shift amount
+  logic [P.LOGNORMSHIFTSZ-1:0]         NormShift;          // normalized result shift amount
+  logic [P.LOGNORMSHIFTSZ-1:0]         DivSubnormShiftAmt; // subnormal result shift amount (killed if negitive)
+  logic [P.NE+1:0]                     DivSubnormShift;    // subnormal result shift amount
 
   // is the result subnormal
   // if the exponent is 1 then the result needs to be normalized then the result is Subnormalizes
-  assign DivResSubnorm = DivQe[`NE+1]|(~|DivQe[`NE+1:0]);
+  assign DivResSubnorm = DivQe[P.NE+1]|(~|DivQe[P.NE+1:0]);
 
   // if the result is subnormal
   //  00000000x.xxxxxx...                     Exp = DivQe
@@ -51,8 +49,8 @@ module divshiftcalc(
   //  .00xxxxxxxxxxxxx... << DivQe+NF+1  Exp = +1
   //  .0000xxxxxxxxxxx... >> 1                Exp = 1
   // Left shift amount  = DivQe+NF+1-1
-  assign DivSubnormShift = (`NE+2)'(`NF)+DivQe;
-  assign DivSubnormShiftPos = ~DivSubnormShift[`NE+1];
+  assign DivSubnormShift = (P.NE+2)'(P.NF)+DivQe;
+  assign DivSubnormShiftPos = ~DivSubnormShift[P.NE+1];
 
   // if the result is normalized
   //  00000000x.xxxxxx...                     Exp = DivQe
@@ -62,13 +60,13 @@ module divshiftcalc(
   //  00000000xx.xxxxx... << 1?               Exp = DivQe-1 (determined after)
   // inital Left shift amount  = NF
   // shift one more if the it's a minimally redundent radix 4 - one entire cycle needed for integer bit
-  assign NormShift = (`LOGNORMSHIFTSZ)'(`NF);
+  assign NormShift = (P.LOGNORMSHIFTSZ)'(P.NF);
 
   // if the shift amount is negitive then don't shift (keep sticky bit)
   // need to multiply the early termination shift by LOGR*DIVCOPIES =  left shift of log2(LOGR*DIVCOPIES)
-  assign DivSubnormShiftAmt = DivSubnormShiftPos ? DivSubnormShift[`LOGNORMSHIFTSZ-1:0] : '0;
+  assign DivSubnormShiftAmt = DivSubnormShiftPos ? DivSubnormShift[P.LOGNORMSHIFTSZ-1:0] : '0;
   assign DivShiftAmt = DivResSubnorm ? DivSubnormShiftAmt : NormShift;
 
   // pre-shift the divider result for normalization
-  assign DivShiftIn = {{`NF{1'b0}}, DivQm, {`NORMSHIFTSZ-`DIVb-1-`NF{1'b0}}};
+  assign DivShiftIn = {{P.NF{1'b0}}, DivQm, {P.NORMSHIFTSZ-P.DIVb-1-P.NF{1'b0}}};
 endmodule
