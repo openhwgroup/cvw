c1e7332abfd4bff1a37605a299b1048dcc2862bd
David Harris <David_Harris@hmc.edu>
2023-05-31 06:30:41 -0700

    Fixed paths in creating division test vectors

--- tests/fp/create_all_vectors.sh
+++ tests/fp/create_all_vectors.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 mkdir -p vectors
-#./create_vectors.sh
-#./remove_spaces.sh
+./create_vectors.sh
+./remove_spaces.sh
 
 # to create tvs for evaluation of combined IFdivsqrt
-./combined_IF_vectors/create_IF_vectors.sh
+cd combined_IF_vectors; ./create_IF_vectors.sh
