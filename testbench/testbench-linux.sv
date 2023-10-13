045b0adbbd49c6f2ea1440e174ddc0214542bba7
Rose Thompson <ross1728@gmail.com>
2023-10-13 14:56:45 -0500

    Renamed testbench_imperas.sv to testbench-imperas.sv

--- testbench/testbench-linux.sv
+++ testbench/testbench-linux.sv
@@ -27,7 +27,7 @@
 `include "config.vh"
 import cvw::*;
 
-`define DEBUG_TRACE 0
+`define DEBUG_TRACE 1
 // Debug Levels
 // 0: don't check against QEMU
 // 1: print disagreements with QEMU, but only halt on PCW disagreements
