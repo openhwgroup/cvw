379337fee0c63fdb5af43fb7157a07d607d4d6a5
David Harris <David_Harris@hmc.edu>
2023-10-19 06:16:52 -0700

    Adjusted synthesis scripts to report on DESIGN even when a wrapper is used

--- synthDC/scripts/synth.tcl
+++ synthDC/scripts/synth.tcl
@@ -53,6 +53,7 @@ if { $wrapper == 1 } {
 } else {
     set my_toplevel $::env(DESIGN)
 }
+set my_design $::env(DESIGN)
 
 # Set number of significant digits
 set report_default_significant_digits 6
@@ -238,6 +239,12 @@ set write_rep  1	;# generates estimated area and timing report
 set write_cst  1        ;# generate report of constraints
 set write_hier 1        ;# generate hierarchy report
 
+# Report on DESIGN, not wrapper.  However, design has a suffix for the parameters.
+if { $wrapper == 1 } {
+    set designname [format "%s%s" $my_design "__*"]
+    current_design $designname
+} 
+
 # Report Constraint Violators
 set filename [format "%s%s" $outputDir "/reports/constraint_all_violators.rpt"]
 redirect $filename {report_constraint -all_violators}
@@ -246,16 +253,16 @@ redirect $filename {report_constraint -all_violators}
 redirect $outputDir/reports/check_design.rpt { check_design }
 
 # Report Final Netlist (Hierarchical)
-set filename [format "%s%s%s%s" $outputDir "/mapped/" $my_toplevel ".sv"]
+set filename [format "%s%s%s%s" $outputDir "/mapped/" $my_design ".sv"]
 write_file -f verilog -hierarchy -output $filename
 
-set filename [format "%s%s%s%s" $outputDir "/mapped/" $my_toplevel ".sdc"]
+set filename [format "%s%s%s%s" $outputDir "/mapped/" $my_design ".sdc"]
 write_sdc $filename
 
-set filename [format "%s%s%s%s" $outputDir  "/mapped/" $my_toplevel ".ddc"]
+set filename [format "%s%s%s%s" $outputDir  "/mapped/" $my_design ".ddc"]
 write_file -format ddc -hierarchy -o $filename
 
-set filename [format "%s%s%s%s" $outputDir "/mapped/" $my_toplevel ".sdf"]
+set filename [format "%s%s%s%s" $outputDir "/mapped/" $my_design ".sdf"]
 write_sdf $filename
 
 # QoR
