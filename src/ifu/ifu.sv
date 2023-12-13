c98c0dd3e05c3f81698913e39c2a377fb3077fd4
Rose Thompson <ross1728@gmail.com>
2023-12-13 13:05:10 -0600

    Removed unnecessary pc reset logic from ifu and btb.

--- src/ifu/ifu.sv
+++ src/ifu/ifu.sv
@@ -300,8 +300,8 @@ module ifu import cvw::*;  #(parameter cvw_t P) (
     mux2 #(P.XLEN) pcmux2(.d0(PC1NextF), .d1(NextValidPCE), .s(CSRWriteFenceM),.y(PC2NextF));
   else assign PC2NextF = PC1NextF;
 
-  assign  PCNextF = {UnalignedPCNextF[P.XLEN-1:1], 1'b0}; // hart-SPEC p. 21 about 16-bit alignment
-  flopenl #(P.XLEN) pcreg(clk, reset, ~StallF, PCNextF, P.RESET_VECTOR[P.XLEN-1:0], PCF);
+  mux2 #(P.XLEN) pcresetmux({UnalignedPCNextF[P.XLEN-1:1], 1'b0}, P.RESET_VECTOR[P.XLEN-1:0], reset, PCNextF);
+  flopen #(P.XLEN) pcreg(clk, ~StallF | reset, PCNextF, PCF);
 
   // pcadder
   // add 2 or 4 to the PC, based on whether the instruction is 16 bits or 32
@@ -315,8 +315,7 @@ module ifu import cvw::*;  #(parameter cvw_t P) (
   // an X.  This version of the code does not die because if CompressedF is an X it just defaults to the last
   // option.  The above code would work, but propagates the x.
   always_comb
-    if(reset) PCPlus2or4F = '0;
-    else if (CompressedF) // add 2
+    if (CompressedF) // add 2
       if (PCF[1]) PCPlus2or4F = {PCPlus4F, 2'b00}; 
       else        PCPlus2or4F = {PCF[P.XLEN-1:2], 2'b10};
     else          PCPlus2or4F = {PCPlus4F, PCF[1:0]}; // add 4
