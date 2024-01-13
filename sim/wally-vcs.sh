f9dcc9fb7f28e01a650c9e7ed8e816e87df934a6
Ross Thompson <ross1728@gmail.com>
2024-01-12 22:04:59 -0600

    Stuck on figuring out how to pass parameters to vcs.

--- sim/wally-vcs.sh
+++ sim/wally-vcs.sh
@@ -1,3 +1,7 @@
-vcs -full64 +lint=all,noGCWM -sverilog  +define+ +incdir+../testbench +incdir+../config/rv64gc +incdir+../config/shared ../src/cvw.sv ../testbench/testbench.sv ../testbench/common/DCacheFlushFSM.sv ../testbench/common/checksignature.sv ../testbench/common/functionName.sv ../testbench/common/instr* ../testbench/common/loggers.sv ../testbench/common/ramxdetector.sv ../testbench/common/riscvassertions.sv ../testbench/common/watchdog.sv ../src/*/*.sv ../src/*/*/*.sv ../testbench/vcs-files/*.sv -o simv
+#!/bin/bash
 
-./simv -ucli -do vcs.do
+config=$1
+testsuite=$2
+vcs -full64 +lint=all,noGCWM -sverilog  +define+ +incdir+../testbench +incdir+../config/$config +incdir+../config/shared ../src/cvw.sv ../testbench/testbench.sv ../testbench/common/DCacheFlushFSM.sv ../testbench/common/checksignature.sv ../testbench/common/functionName.sv ../testbench/common/instr* ../testbench/common/loggers.sv ../testbench/common/ramxdetector.sv ../testbench/common/riscvassertions.sv ../testbench/common/watchdog.sv ../src/*/*.sv ../src/*/*/*.sv ../testbench/vcs-files/*.sv -pvalue+TEST=$testsuite -o simv_${config}_$testsuite
+
+./simv_${config}_$testsuite -ucli -do vcs.do
