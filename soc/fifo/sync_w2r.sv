1ea8f99625ca9d8dd5553a1d2839b8fbbf778ad0
Marcus Mellor <infinitymdm@gmail.com>
2024-02-29 11:13:56 -0600

    soc/fifo: Add copyright headers

--- soc/fifo/sync_w2r.sv
+++ soc/fifo/sync_w2r.sv
@@ -1,3 +1,31 @@
+///////////////////////////////////////////
+// sync_w2r.sv
+//
+// Written: Clifford E Cummings 16 June 2005
+// Modified: james.stine@okstate.edu 19 February 2024
+//
+// Purpose: FIFO write-domain to read-domain synchronizer
+// 
+// Documentation: 
+//
+// A component of the CORE-V-WALLY configurable RISC-V project.
+// https://github.com/openhwgroup/cvw
+// 
+// Copyright (C) 2021-24 Harvey Mudd College & Oklahoma State University
+//
+// SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+//
+// Licensed under the Solderpad Hardware License v 2.1 (the “License”); you may not use this file 
+// except in compliance with the License, or, at your option, the Apache License version 2.0. You 
+// may obtain a copy of the License at
+//
+// https://solderpad.org/licenses/SHL-2.1/
+//
+// Unless required by applicable law or agreed to in writing, any work distributed under the 
+// License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+// either express or implied. See the License for the specific language governing permissions 
+// and limitations under the License.
+////////////////////////////////////////////////////////////////////////////////////////////////
 module sync_w2r #(parameter ADDRSIZE = 4)
    (rq2_wptr, wptr, rclk, rrst_n);
 
