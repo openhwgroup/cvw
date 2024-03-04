031c6b28111a394a7b18d9228a9b62e1f093ce18
Ross Thompson <ross1728@gmail.com>
2024-03-04 17:55:23 -0600

    changes for vcs.

--- testbench/vcs-files/memory-wrappers.sv
+++ testbench/vcs-files/memory-wrappers.sv
@@ -1,20 +1,3 @@
-module TSDN28HPCPA128X64M4FW( 
-  input  logic          CLKA, 
-  input  logic          CLKB, 
-  input  logic          CEBA, 
-  input  logic          CEBB, 
-  input  logic          WEBA,
-  input  logic          WEBB,
-  input  logic [6:0]    AA, 
-  input  logic [6:0]    AB, 
-  input  logic [63:0]   DA,
-  input  logic [63:0]   DB,
-  input  logic [63:0]   BWEBA, 
-  input  logic [63:0]   BWEBB, 
-  output logic [63:0]   QA,
-  output logic [63:0]   QB
-);
-endmodule
 
 module TSDN28HPCPA2048X64MMFW( 
   input  logic          CLKA, 
