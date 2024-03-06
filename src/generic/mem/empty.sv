120b48d04a3613655f343ee042c4861b365480bf
Ross Thompson <ross1728@gmail.com>
2024-03-06 17:00:29 -0600

    required for vcs.  we need to remove this empty file asap.

---
+++ src/generic/mem/empty.sv
@@ -0,0 +1,44 @@
+///////////////////////////////////////////
+// ram2p1rwbe_128x64.sv
+//
+// Written: Rose Thompon ross1728@gmail.com 06 March 2024
+// Modified: 
+//
+// Purpose: Empty wrapper for VCS to work.  Would really like to not have any of these.
+// 
+// A component of the CORE-V-WALLY configurable RISC-V project.
+// 
+// Copyright (C) 2021-23 Harvey Mudd College & Oklahoma State University
+//
+// SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+//
+// Licensed under the Solderpad Hardware License v 2.1 (the “License”); you may not use this file 
+// except in compliance with the License, or, at your option, the Apache License version 2.0. You 
+// may obtain a copy of the License at
+//
+// https://solderpad.org/licenses/SHL-2.1/
+//
+// Unless required by applicable law or agreed to in writing, any work distributed under the 
+// License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+// either express or implied. See the License for the specific language governing permissions 
+// and limitations under the License.
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+module TSDN28HPCPA128X64M4FW( 
+  input  logic          CLKA, 
+  input  logic          CLKB, 
+  input  logic          CEBA, 
+  input  logic          CEBB, 
+  input  logic          WEBA,
+  input  logic          WEBB,
+  input  logic [6:0]    AA, 
+  input  logic [6:0]    AB, 
+  input  logic [63:0]   DA,
+  input  logic [63:0]   DB,
+  input  logic [63:0]   BWEBA, 
+  input  logic [63:0]   BWEBB, 
+  output logic [63:0]   QA,
+  output logic [63:0]   QB
+);
+
+endmodule
