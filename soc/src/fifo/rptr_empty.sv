ce25816e44a1584deeb525606d67c55e60c2b044
Marcus Mellor <infinitymdm@gmail.com>
2024-03-06 16:12:03 -0600

    soc/src: Fix small FIFO bugs, correct DDR paramaterization

--- soc/src/fifo/rptr_empty.sv
+++ soc/src/fifo/rptr_empty.sv
@@ -2,7 +2,7 @@
 // rptr_empty.sv
 //
 // Written: Clifford E Cummings 16 June 2005
-// Modified: james.stine@okstate.edu 19 February 2024
+// Modified: infinitymdm@gmail.com 5 March 2024
 //
 // Purpose: FIFO read pointer and empty generation logic
 // 
@@ -27,39 +27,40 @@
 // and limitations under the License.
 ////////////////////////////////////////////////////////////////////////////////////////////////
 module rptr_empty #(parameter ADDRSIZE = 4)
-   (rempty, raddr, rptr, rq2_wptr, rinc, rclk, rrst_n);
+  (rempty, raddr, rptr, rq2_wptr, rinc, rclk, rrst_n);
 
-   input logic [ADDRSIZE:0]    rq2_wptr;
-   input logic 		       rinc;
-   input logic 		       rclk;
-   input logic 		       rrst_n;   
-   output logic 	       rempty;
-   output logic [ADDRSIZE-1:0] raddr;
-   output logic [ADDRSIZE  :0] rptr;
+  input  logic [ADDRSIZE:0]   rq2_wptr;
+  input  logic                rinc;
+  input  logic                rclk;
+  input  logic                rrst_n;
+  output logic                rempty;
+  output logic [ADDRSIZE-1:0] raddr;
+  output logic [ADDRSIZE  :0] rptr;
 
-   logic [ADDRSIZE:0] 	       rbin;
-   logic [ADDRSIZE:0] 	       rgraynext;
-   logic [ADDRSIZE:0] 	       rbinnext;   
+  logic               rempty_val;
+  logic [ADDRSIZE:0]  rbin;
+  logic [ADDRSIZE:0]  rgraynext;
+  logic [ADDRSIZE:0]  rbinnext;
 
-   //-------------------
-   // GRAYSTYLE2 pointer
-   //-------------------
-   always @(posedge rclk or negedge rrst_n)
-     if (!rrst_n) {rbin, rptr} <= 0;
-     else         {rbin, rptr} <= {rbinnext, rgraynext};
-   
-   // Memory read-address pointer (okay to use binary to address memory)
-   assign raddr     = rbin[ADDRSIZE-1:0];
-   assign rbinnext  = rbin + (rinc & ~rempty);
-   assign rgraynext = (rbinnext>>1) ^ rbinnext;
-   
-   //--------------------------------------------------------------- 
-   // FIFO empty when the next rptr == synchronized wptr or on reset 
-   //--------------------------------------------------------------- 
-   assign rempty_val = (rgraynext == rq2_wptr);
-   
-   always @(posedge rclk or negedge rrst_n)
-     if (!rrst_n) rempty <= 1'b1;   
-     else         rempty <= rempty_val;
-   
+  //-------------------
+  // GRAYSTYLE2 pointer
+  //-------------------
+  always @(posedge rclk or negedge rrst_n)
+    if (!rrst_n) {rbin, rptr} <= 0;
+    else         {rbin, rptr} <= {rbinnext, rgraynext};
+
+  // Memory read-address pointer (okay to use binary to address memory)
+  assign raddr     = rbin[ADDRSIZE-1:0];
+  assign rbinnext  = rbin + (rinc & ~rempty);
+  assign rgraynext = (rbinnext>>1) ^ rbinnext;
+
+  //--------------------------------------------------------------- 
+  // FIFO empty when the next rptr == synchronized wptr or on reset 
+  //--------------------------------------------------------------- 
+  assign rempty_val = (rgraynext == rq2_wptr);
+
+  always @(posedge rclk or negedge rrst_n)
+    if (!rrst_n) rempty <= 1'b1;   
+    else         rempty <= rempty_val;
+
 endmodule // rptr_empty
