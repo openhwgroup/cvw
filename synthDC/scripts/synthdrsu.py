33807187febcf706811e7c2f12b8dd367808ff87
Kevin Kim <kevindkim723@gmail.com>
2024-03-25 10:09:00 -0700

    batch synthesis script updates

--- synthDC/scripts/synthdrsu.py
+++ synthDC/scripts/synthdrsu.py
@@ -1,9 +1,21 @@
-import os
+import sys,os,shutil,multiprocessing
 from multiprocessing import Pool, TimeoutError
+from collections import namedtuple
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-Job = namedtuple("Job", ['config', 'frequency', 'cmd', 'grepstr'])
-configs = [
+regressionDir = os.path.dirname(os.path.join(os.path.abspath(__file__),os.pardir))
+Job = namedtuple("Job", ['name', 'frequency', 'cmd', 'grepstr'])
+derivedconfigs = [
     "fd_ieee_div_2_1i_rv32gc", "fd_ieee_div_2_1i_rv64gc", "fd_ieee_div_2_1_rv32gc",
     "fd_ieee_div_2_1_rv64gc", "fd_ieee_div_2_2i_rv32gc", "fd_ieee_div_2_2i_rv64gc",
     "fd_ieee_div_2_2_rv32gc", "fd_ieee_div_2_2_rv64gc", "fd_ieee_div_2_4i_rv32gc",
@@ -32,43 +44,50 @@ configs = [
 
 def search_log_for_text(text, logfile):
     """Search through the given log file for text, returning True if it is found or False if it is not"""
-    grepcmd = "grep -e '%s' '%s' > /dev/null" % (text, logfile)
+    print(os.getcwd())
+    grepcmd = "grep -e '%s' '../%s' > /dev/null" % (text, logfile)
     return os.system(grepcmd) == 0
 
 def run_synth_job(config):
     """Run the given test case, and return 0 if the test suceeds and 1 if it fails"""
-    logname = "logs/"+config.variant+"_"+config.name+".log"
+    logname = "runs/"+config.name+".log"
+    logname = f"runs/drsu_{config.name}_orig_tsmc28nm_{config.frequency}_MHz/synth.out"
     cmd = config.cmd
 #    print(cmd)
     os.system(cmd)
     if search_log_for_text(config.grepstr, logname):
-        print(f"{bcolors.OKGREEN}%s_%s: Success{bcolors.ENDC}" % (config.variant, config.name))
+        print(f"{bcolors.OKGREEN}%s_%s: Success{bcolors.ENDC}" % (config.name, config.frequency))
         return 0
     else:
-        print(f"{bcolors.FAIL}%s_%s: Failures detected in output{bcolors.ENDC}" % (config.variant, config.name))
+        print(f"{bcolors.FAIL}%s_%s: Failures detected in output{bcolors.ENDC}" % (config.name, config.frequency))
         print("  Check %s" % logname)
         return 1
 
 def main():
+
+    TIMEOUT_DUR = 30*7200 # seconds
     """Run the tests and count the failures"""
-    global configs, coverage
+    global derivedconfigs, coverage
+    configs=[]
     try:
         os.chdir(regressionDir)
         os.mkdir("logs")
     except:
         pass
 
-    for config in configs:
+    for config in derivedconfigs:
         synthjob_5000 = Job(
             name=config,
-            cmd=f"make -C $WALLY/synthDC synth DESIGN=drsu TECH=tsmc28 CONFIG={config} FREQ=5000 > /dev/null" 
-            grepstr="All Tests completed with          0 errors" # change this
+            cmd=f"make -C $WALLY/synthDC synth DESIGN=drsu TECH=tsmc28 CONFIG={config} FREQ=5000 > /dev/null", 
+            grepstr="Optimization",
+            frequency=5000
         )
 
         synthjob_100 = Job(
             name=config,
-            cmd=f"make -C $WALLY/synthDC synth DESIGN=drsu TECH=tsmc28 CONFIG={config} FREQ=100 > /dev/null" 
-            grepstr="All Tests completed with          0 errors" # change this
+            cmd=f"make -C $WALLY/synthDC synth DESIGN=drsu TECH=tsmc28 CONFIG={config} FREQ=100 > /dev/null", 
+            grepstr="Optimization", 
+            frequency=100
         )
         configs.append(synthjob_5000)
         configs.append(synthjob_100)
