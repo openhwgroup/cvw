3bd80f2b3a73e5763f527939789ac6186dff4a57
Kevin Kim <kevindkim723@gmail.com>
2024-03-26 11:26:42 -0700

    synth flow update

--- synthDC/scripts/parseCSV.py
+++ synthDC/scripts/parseCSV.py
@@ -36,55 +36,67 @@ def parse_r_k(rows,freq):
     rowout = []
     for row in rows:
         design = row[0]
+        r = int(design.split("_")[4])
+        k = int(design.split("_")[5][0])
         area = row[1]
         delay = row[2]
         power = float(row[3])/freq
-        if "RADIX_2" in design and "K_1" in design and len(rowout)/3 == 0:
+        if r==2 and k==1 and len(rowout)/3 == 0:
             rowout.append(area)
             rowout.append(delay)
             rowout.append(power)
     for row in rows:
         design = row[0]
+        r = int(design.split("_")[4])
+        k = int(design.split("_")[5][0])
         area = row[1]
         delay = row[2]
         power = float(row[3])/freq
-        if "RADIX_2" in design and "K_2" in design and len(rowout)/3 == 1:
+        if r==2 and k==2 and len(rowout)/3 == 1:
             rowout.append(area)
             rowout.append(delay)
             rowout.append(power)
     for row in rows:
         design = row[0]
+        r = int(design.split("_")[4])
+        k = int(design.split("_")[5][0])
         area = row[1]
         delay = row[2]
         power = float(row[3])/freq
-        if "RADIX_2" in design and "K_4" in design and len(rowout)/3==2:
+        if r==2 and k==4  and len(rowout)/3==2:
             rowout.append(area)
             rowout.append(delay)
             rowout.append(power)
     for row in rows:
         design = row[0]
+        r = int(design.split("_")[4])
+        k = int(design.split("_")[5][0])
         area = row[1]
         delay = row[2]
         power = float(row[3])/freq
-        if "RADIX_4" in design and "K_1" in design and len(rowout)/3 ==3:
+        if r==4 and k== 1 and len(rowout)/3 ==3:
             rowout.append(area)
             rowout.append(delay)
             rowout.append(power)
     for row in rows:
         design = row[0]
+        r = int(design.split("_")[4])
+        k = int(design.split("_")[5][0])
         area = row[1]
         delay = row[2]
         power = float(row[3])/freq
-        if "RADIX_4" in design and "K_2" in design and len(rowout)/3 ==4:
+        if r==4 and k==2 and len(rowout)/3 ==4:
             rowout.append(area)
             rowout.append(delay)
             rowout.append(power)
     for row in rows:
         design = row[0]
+        r = int(design.split("_")[4])
+        k = int(design.split("_")[5][0])
         area = row[1]
         delay = row[2]
         power = float(row[3])/freq
-        if "RADIX_4" in design and "K_4" in design and len(rowout)/3 ==5:
+        if r==4 and k==4 and len(rowout)/3 ==5:
             rowout.append(area)
             rowout.append(delay)
             rowout.append(power)
@@ -92,22 +104,37 @@ def parse_r_k(rows,freq):
     return rowout
 
 def titleClean(title):
-    i = title.index("RADIX")
-    title=title[:i] + title[i+12:]
-    i = title.index("IDIVBITS")
-    title=title[:i] + title[i+11:]
-    return title
+  
+  tokens = title.split("_")
+  tokens.pop(3)
+  tokens.pop(3)
+  tokens.pop(3)
+  tokens.pop(4)
+  if "i_" in title:
+    title = "_".join(tokens) + "_IDIV"
+  else:
+    title = "_".join(tokens)
+
+  """
+
+  i = title.index("RADIX")
+  title=title[:i] + title[i+12:]
+  i = title.index("IDIVBITS")
+  title=title[:i] + title[i+11:]
+  """
+  return title
 def parse_fpmode(rows,freq):
     rowf = []
     rowd = []
     rowq = []
     rowout = []
     for row in rows:
-        if "FPMODE_F" in row[0]:
+        precision = row[0].split("_")[1]
+        if precision == "f":
             rowf.append(row)
-        if "FPMODE_D" in row[0]:
+        if precision == "fd":
             rowd.append(row)
-        if "FPMODE_Q" in row[0]:
+        if precision == "fdq":
             rowq.append(row)
     rowout.append(parse_r_k(rowf,freq))
     rowout.append(parse_r_k(rowd,freq))
@@ -121,10 +148,13 @@ def parse_idiv(rows,freq):
     noidivrows = []
     rowout = []
     for row in rows:
-        if "INTDIV_1" in row[0]:
-            idivrows.append(row)
-        else:
-            noidivrows.append(row)
+      print(row)
+      idiv = len((row[0].split("_"))[5]) == 2
+      print(idiv)
+      if idiv:
+          idivrows.append(row)
+      else:
+          noidivrows.append(row)
     rowout.extend(parse_xlen(idivrows,freq))
     rowout.extend(parse_xlen(noidivrows,freq))
     return rowout
