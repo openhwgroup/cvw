70a6f39ab67a58596465c0ec2408961bfecfa71c
Marcus Mellor <infinitymdm@gmail.com>
2024-03-27 14:39:45 -0500

    soc: Fix simulation for non-soc configs
    Add a preprocessor ifdef to prevent bsg_dmc-related stuff from being included in non-soc synthesis and simulation. This fixes errors when building non-soc configs.

--- testbench/testbench.sv
+++ testbench/testbench.sv
@@ -554,116 +554,118 @@ module testbench;
 	generate
 	  if(P.EXT_MEM_SUPPORTED) begin
 	  	if (P.USE_BSG_DMC) begin
-	  		`include "bsg_dmc.svh" // FIXME: This should be behind a preprocessor ifdef
-	  		import bsg_dmc_pkg::bsg_dmc_s; // FIXME: This should be behind a preprocessor ifdef
-	  		localparam dq_width = P.XLEN;
-	  		localparam dq_group = dq_width/8;
-	  		logic                 ui_clk;
-	  		logic                 ddr_ck_p;
-  		  logic                 ddr_ck_n;
-  		  logic                 ddr_cke;
-  		  logic [2:0]           ddr_ba;
-  		  logic [15:0]          ddr_addr;
-  		  logic                 ddr_cs;
-  		  logic                 ddr_ras;
-  		  logic                 ddr_cas;
-  		  logic                 ddr_we;
-  		  logic                 ddr_reset;
-  		  logic                 ddr_odt;
-  		  logic [dq_group-1:0]  ddr_dm_oen_o;
-  		  logic [dq_group-1:0]  ddr_dm;
-  		  logic [dq_group-1:0]  ddr_dm_o;
-  		  logic [dq_group-1:0]  ddr_dqs_p_oen_o;
-  		  logic [dq_group-1:0]  ddr_dqs_p_ien_o;
-  		  logic [dq_group-1:0]  ddr_dqs_p;
-  		  logic [dq_group-1:0]  ddr_dqs_p_o;
-  		  logic [dq_group-1:0]  ddr_dqs_p_i;
-  		  logic [dq_group-1:0]  ddr_dqs_n_oen_o;
-  		  logic [dq_group-1:0]  ddr_dqs_n_ien_o;
-  		  logic [dq_group-1:0]  ddr_dqs_n;
-  		  logic [dq_group-1:0]  ddr_dqs_n_o;
-  		  logic [dq_group-1:0]  ddr_dqs_n_i;
-  		  logic [dq_width-1:0]  ddr_dq_oen_o;
-  		  logic [dq_width-1:0]  ddr_dq;
-  		  logic [dq_width-1:0]  ddr_dq_o;
-  		  logic [dq_width-1:0]  ddr_dq_i;
-  		  logic                 dfi_clk_2x_i;
-  		  logic                 dfi_clk_1x_o;
-  		  always #2.5ns ui_clk = ~ui_clk; // ui_clk must be <= 208 MHz
-  		  always #1.25ns dfi_clk_2x_i = ~dfi_clk_2x_i; // dfi_clk_2x must be 2x ui_clk
-  		  // Initialize bsg_dmc
-			  bsg_dmc_s dmc_config;
-			  initial begin: bsg_dmc_config
-			    dmc_config.trefi = 1023;
-			    dmc_config.tmrd = 1;
-			    dmc_config.trfc = 15;
-			    dmc_config.trc = 10;
-			    dmc_config.trp = 2;
-			    dmc_config.tras = 7;
-			    dmc_config.trrd = 1;
-			    dmc_config.trcd = 2;
-			    dmc_config.twr = 10;
-			    dmc_config.twtr = 7;
-			    dmc_config.trtp = 10;
-			    dmc_config.tcas = 3;
-			    dmc_config.col_width = 11;
-			    dmc_config.row_width = 14;
-			    dmc_config.bank_width = 2;
-			    dmc_config.dqs_sel_cal = 3;
-			    dmc_config.init_cycles = 40010;
-			    dmc_config.bank_pos = 25;
-			    force ram.dmc.dmc_clk_rst_gen.btc_async_reset.tag_data_reg.data_r = 0;
-			    force ram.dmc.dmc_clk_rst_gen.dly_lines[0].dly_line_inst.ctrl_rrr = 31;
-			    force ram.dmc.dmc_clk_rst_gen.dly_lines[1].dly_line_inst.ctrl_rrr = 31;
-			    force ram.dmc.dmc_clk_rst_gen.dly_lines[2].dly_line_inst.ctrl_rrr = 31;
-			    force ram.dmc.dmc_clk_rst_gen.dly_lines[3].dly_line_inst.ctrl_rrr = 31;
-			    force ram.dmc.dmc_clk_rst_gen.clk_gen_ds_inst.reset_i = 1'b1;
-			    force ram.dmc.dmc_clk_rst_gen.clk_gen_ds_inst.strobe_r = 1'b0;
-			    ui_clk = 1'b0;
-			    dfi_clk_2x_i = 1'b0;
-			    #100ns;
-			    force ram.dmc.dmc_clk_rst_gen.clk_gen_ds_inst.reset_i = 1'b0;
-			    force ram.dmc.dmc_clk_rst_gen.clk_gen_ds_inst.strobe_r = 1'b1;
-			  end
-			  bsg_dmc_ahb #(28, P.XLEN, 64) ram (
-		      .dmc_config,
-		      .HCLK, .HRESETn, .HSEL(HSELEXT),
-		      .HADDR(HADDR[27:0]), .HWDATA, .HWSTRB,
-		      .HWRITE, .HTRANS, .HREADY,
-		      .HRDATA(HRDATAEXT), .HRESP(HRESPEXT), .HREADYOUT(HREADYEXT),
-		      .ui_clk,
-		      .ddr_ck_p, .ddr_ck_n, .ddr_cke, .ddr_ba, .ddr_addr,
-		      .ddr_cs, .ddr_ras, .ddr_cas, .ddr_we, .ddr_reset, .ddr_odt,
-		      .ddr_dm_oen(ddr_dm_oen_o), .ddr_dm(ddr_dm_o),
-		      .ddr_dqs_p_oen(ddr_dqs_p_oen_o), .ddr_dqs_p_ien(ddr_dqs_p_ien_o),
-		      .ddr_dqs_p_out(ddr_dqs_p_o), .ddr_dqs_p_in(ddr_dqs_p_i),
-		      .ddr_dqs_n_oen(ddr_dqs_n_oen_o), .ddr_dqs_n_ien(ddr_dqs_n_ien_o),
-		      .ddr_dqs_n_out(ddr_dqs_n_o), .ddr_dqs_n_in(ddr_dqs_n_i),
-		      .ddr_dq_oen(ddr_dq_oen_o), .ddr_dq_out(ddr_dq_o), .ddr_dq_in(ddr_dq_i),
-		      .dfi_clk_2x(dfi_clk_2x_i), .dfi_clk_1x(dfi_clk_1x_o));
-  		  for(ddr_i = 0; ddr_i < dq_group; ddr_i++) begin: dm_io
-	        assign ddr_dm[ddr_i] = !ddr_dm_oen_o[ddr_i]? ddr_dm_o[ddr_i]: 1'bz;
-	      end
-	      for(ddr_i = 0; ddr_i < dq_group; ddr_i++) begin: dqs_io
-	        assign ddr_dqs_p[ddr_i]   = !ddr_dqs_p_oen_o[ddr_i]? ddr_dqs_p_o[ddr_i]: 1'bz;
-	        assign ddr_dqs_p_i[ddr_i] = !ddr_dqs_p_ien_o[ddr_i]? ddr_dqs_p[ddr_i]: 1'b0;
-	        assign ddr_dqs_n[ddr_i]   = !ddr_dqs_n_oen_o[ddr_i]? ddr_dqs_n_o[ddr_i]: 1'bz;
-	        assign ddr_dqs_n_i[ddr_i] = !ddr_dqs_n_ien_o[ddr_i]? ddr_dqs_n[ddr_i]: 1'b1;
-	      end
-	      for(ddr_i = 0; ddr_i < dq_width; ddr_i++) begin: dq_io
-	        assign ddr_dq[ddr_i]    = !ddr_dq_oen_o[ddr_i]? ddr_dq_o[ddr_i]: 1'bz;
-	        assign ddr_dq_i[ddr_i]  = ddr_dq[ddr_i];
-	      end
-	      for(ddr_i = 0; ddr_i < 2; ddr_i++) begin: lpddr
-	        mobile_ddr mobile_ddr_inst (
-	        	.Dq(ddr_dq[dq_width/2*(ddr_i+1)-1:dq_width/2*ddr_i]),
-	        	.Dqs(ddr_dqs_p[dq_group/2*(ddr_i+1)-1:dq_group/2*ddr_i]),
-	          .Addr(ddr_addr[13:0]), .Ba(ddr_ba[1:0]), .Clk(ddr_ck_p), .Clk_n(ddr_ck_n),
-	          .Cke(ddr_cke), .Cs_n(ddr_cs), .Ras_n(ddr_ras), .Cas_n(ddr_cas), .We_n(ddr_we),
-	          .Dm(ddr_dm[dq_group/2*(ddr_i+1)-1:dq_group/2*ddr_i]));
-	      end
-	  	end else begin
-        ram_ahb #(.P(P), .BASE(P.EXT_MEM_BASE), .RANGE(P.EXT_MEM_RANGE)) 
+	  		`ifdef USE_BSG
+          `include "bsg_dmc.svh"
+          import bsg_dmc_pkg::bsg_dmc_s;
+          localparam dq_width = P.XLEN;
+          localparam dq_group = dq_width/8;
+          logic                 ui_clk;
+          logic                 ddr_ck_p;
+          logic                 ddr_ck_n;
+          logic                 ddr_cke;
+          logic [2:0]           ddr_ba;
+          logic [15:0]          ddr_addr;
+          logic                 ddr_cs;
+          logic                 ddr_ras;
+          logic                 ddr_cas;
+          logic                 ddr_we;
+          logic                 ddr_reset;
+          logic                 ddr_odt;
+          logic [dq_group-1:0]  ddr_dm_oen_o;
+          logic [dq_group-1:0]  ddr_dm;
+          logic [dq_group-1:0]  ddr_dm_o;
+          logic [dq_group-1:0]  ddr_dqs_p_oen_o;
+          logic [dq_group-1:0]  ddr_dqs_p_ien_o;
+          logic [dq_group-1:0]  ddr_dqs_p;
+          logic [dq_group-1:0]  ddr_dqs_p_o;
+          logic [dq_group-1:0]  ddr_dqs_p_i;
+          logic [dq_group-1:0]  ddr_dqs_n_oen_o;
+          logic [dq_group-1:0]  ddr_dqs_n_ien_o;
+          logic [dq_group-1:0]  ddr_dqs_n;
+          logic [dq_group-1:0]  ddr_dqs_n_o;
+          logic [dq_group-1:0]  ddr_dqs_n_i;
+          logic [dq_width-1:0]  ddr_dq_oen_o;
+          logic [dq_width-1:0]  ddr_dq;
+          logic [dq_width-1:0]  ddr_dq_o;
+          logic [dq_width-1:0]  ddr_dq_i;
+          logic                 dfi_clk_2x_i;
+          logic                 dfi_clk_1x_o;
+          always #2.5ns ui_clk = ~ui_clk; // ui_clk must be <= 208 MHz
+          always #1.25ns dfi_clk_2x_i = ~dfi_clk_2x_i; // dfi_clk_2x must be 2x ui_clk
+          // Initialize bsg_dmc
+          bsg_dmc_s dmc_config;
+          initial begin: bsg_dmc_config
+            dmc_config.trefi = 1023;
+            dmc_config.tmrd = 1;
+            dmc_config.trfc = 15;
+            dmc_config.trc = 10;
+            dmc_config.trp = 2;
+            dmc_config.tras = 7;
+            dmc_config.trrd = 1;
+            dmc_config.trcd = 2;
+            dmc_config.twr = 10;
+            dmc_config.twtr = 7;
+            dmc_config.trtp = 10;
+            dmc_config.tcas = 3;
+            dmc_config.col_width = 11;
+            dmc_config.row_width = 14;
+            dmc_config.bank_width = 2;
+            dmc_config.dqs_sel_cal = 3;
+            dmc_config.init_cycles = 40010;
+            dmc_config.bank_pos = 25;
+            force ram.dmc.dmc_clk_rst_gen.btc_async_reset.tag_data_reg.data_r = 0;
+            force ram.dmc.dmc_clk_rst_gen.dly_lines[0].dly_line_inst.ctrl_rrr = 31;
+            force ram.dmc.dmc_clk_rst_gen.dly_lines[1].dly_line_inst.ctrl_rrr = 31;
+            force ram.dmc.dmc_clk_rst_gen.dly_lines[2].dly_line_inst.ctrl_rrr = 31;
+            force ram.dmc.dmc_clk_rst_gen.dly_lines[3].dly_line_inst.ctrl_rrr = 31;
+            force ram.dmc.dmc_clk_rst_gen.clk_gen_ds_inst.reset_i = 1'b1;
+            force ram.dmc.dmc_clk_rst_gen.clk_gen_ds_inst.strobe_r = 1'b0;
+            ui_clk = 1'b0;
+            dfi_clk_2x_i = 1'b0;
+            #100ns;
+            force ram.dmc.dmc_clk_rst_gen.clk_gen_ds_inst.reset_i = 1'b0;
+            force ram.dmc.dmc_clk_rst_gen.clk_gen_ds_inst.strobe_r = 1'b1;
+          end
+          bsg_dmc_ahb #(28, P.XLEN, 64) ram (
+            .dmc_config,
+            .HCLK, .HRESETn, .HSEL(HSELEXT),
+            .HADDR(HADDR[27:0]), .HWDATA, .HWSTRB,
+            .HWRITE, .HTRANS, .HREADY,
+            .HRDATA(HRDATAEXT), .HRESP(HRESPEXT), .HREADYOUT(HREADYEXT),
+            .ui_clk,
+            .ddr_ck_p, .ddr_ck_n, .ddr_cke, .ddr_ba, .ddr_addr,
+            .ddr_cs, .ddr_ras, .ddr_cas, .ddr_we, .ddr_reset, .ddr_odt,
+            .ddr_dm_oen(ddr_dm_oen_o), .ddr_dm(ddr_dm_o),
+            .ddr_dqs_p_oen(ddr_dqs_p_oen_o), .ddr_dqs_p_ien(ddr_dqs_p_ien_o),
+            .ddr_dqs_p_out(ddr_dqs_p_o), .ddr_dqs_p_in(ddr_dqs_p_i),
+            .ddr_dqs_n_oen(ddr_dqs_n_oen_o), .ddr_dqs_n_ien(ddr_dqs_n_ien_o),
+            .ddr_dqs_n_out(ddr_dqs_n_o), .ddr_dqs_n_in(ddr_dqs_n_i),
+            .ddr_dq_oen(ddr_dq_oen_o), .ddr_dq_out(ddr_dq_o), .ddr_dq_in(ddr_dq_i),
+            .dfi_clk_2x(dfi_clk_2x_i), .dfi_clk_1x(dfi_clk_1x_o));
+          for(ddr_i = 0; ddr_i < dq_group; ddr_i++) begin: dm_io
+            assign ddr_dm[ddr_i] = !ddr_dm_oen_o[ddr_i]? ddr_dm_o[ddr_i]: 1'bz;
+          end
+          for(ddr_i = 0; ddr_i < dq_group; ddr_i++) begin: dqs_io
+            assign ddr_dqs_p[ddr_i]   = !ddr_dqs_p_oen_o[ddr_i]? ddr_dqs_p_o[ddr_i]: 1'bz;
+            assign ddr_dqs_p_i[ddr_i] = !ddr_dqs_p_ien_o[ddr_i]? ddr_dqs_p[ddr_i]: 1'b0;
+            assign ddr_dqs_n[ddr_i]   = !ddr_dqs_n_oen_o[ddr_i]? ddr_dqs_n_o[ddr_i]: 1'bz;
+            assign ddr_dqs_n_i[ddr_i] = !ddr_dqs_n_ien_o[ddr_i]? ddr_dqs_n[ddr_i]: 1'b1;
+          end
+          for(ddr_i = 0; ddr_i < dq_width; ddr_i++) begin: dq_io
+            assign ddr_dq[ddr_i]    = !ddr_dq_oen_o[ddr_i]? ddr_dq_o[ddr_i]: 1'bz;
+            assign ddr_dq_i[ddr_i]  = ddr_dq[ddr_i];
+          end
+          for(ddr_i = 0; ddr_i < 2; ddr_i++) begin: lpddr
+            mobile_ddr mobile_ddr_inst (
+              .Dq(ddr_dq[dq_width/2*(ddr_i+1)-1:dq_width/2*ddr_i]),
+              .Dqs(ddr_dqs_p[dq_group/2*(ddr_i+1)-1:dq_group/2*ddr_i]),
+              .Addr(ddr_addr[13:0]), .Ba(ddr_ba[1:0]), .Clk(ddr_ck_p), .Clk_n(ddr_ck_n),
+              .Cke(ddr_cke), .Cs_n(ddr_cs), .Ras_n(ddr_ras), .Cas_n(ddr_cas), .We_n(ddr_we),
+              .Dm(ddr_dm[dq_group/2*(ddr_i+1)-1:dq_group/2*ddr_i]));
+          end
+        `endif
+	    end else begin
+        ram_ahb #(.P(P), .BASE(P.EXT_MEM_BASE), .RANGE(P.EXT_MEM_RANGE))
         ram (.HCLK, .HRESETn, .HADDR, .HWRITE, .HTRANS, .HWDATA, .HSELRam(HSELEXT), 
           .HREADRam(HRDATAEXT), .HREADYRam(HREADYEXT), .HRESPRam(HRESPEXT), .HREADY, .HWSTRB);
 	    end
