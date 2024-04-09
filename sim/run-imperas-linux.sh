90040a6a217ae38c61b95d17107898833e2b3b42
slmnemo <nicholas.lucioforlife@yahoo.com>
2024-04-09 16:13:31 -0700

    Added extra path to run-imperas-linux.sh to match new questa directory with .do files

--- sim/run-imperas-linux.sh
+++ sim/run-imperas-linux.sh
@@ -7,4 +7,4 @@ export OTHERFLAGS="+TRACE2LOG_ENABLE=1 +TRACE2LOG_AFTER=100"
 #export OTHERFLAGS="+TRACE2LOG_ENABLE=1  +TRACE2LOG_AFTER=10500000"
 #export OTHERFLAGS=""
 
-vsim -c -do "do wally-linux-imperas.do buildroot buildroot $::env(RISCV) 0 0 0"
+vsim -c -do "do questa/wally-linux-imperas.do buildroot buildroot $::env(RISCV) 0 0 0"
