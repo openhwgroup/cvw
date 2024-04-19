d46e79b7612491f90ef298eae7b00be8d91f0e05
Matthew Otto <106996253+Matthew-Otto@users.noreply.github.com>
2024-04-19 15:51:50 -0500

    update access register function

--- src/debug/hw_interface.py
+++ src/debug/hw_interface.py
@@ -35,25 +35,27 @@ debug = True
 
 def main():
     global tn
-    with Telnet("127.0.0.1", 4444) as tn:
-        activate_dm()
-        abstract_cmd(0,0,0)
-        print(f"DATA0: {read_data(0)}")
-        print(f"DATA1: {read_data(1)}")
-        print("Writing 00000000_BAADFEED to R1")
-        write_data(1, "0x0")
-        write_data(0, "0xbaadfeed")
-        abstract_cmd(0,1,0)
+    #with Telnet("127.0.0.1", 4444) as tn:
+    #activate_dm()
+    access_register(32,1,0)
+    print(f"DATA0: {read_data(0)}")
+    print(f"DATA1: {read_data(1)}")
+    print("Writing 00000000_BAADFEED to R1")
+    write_data(1, "0x0")
+    write_data(0, "0xbaadfeed")
+    access_register(0,1,0)
         
 
 
 
 
 def write_data(regno, data):
+    """Writes to a message register DATA0-"""
     if regno == 1:
         write_dmi("0x5", data)
     elif regno == 0:
         write_dmi("0x4", data)
+
 
 def read_data(regno):
     if regno == 1:
@@ -61,47 +63,60 @@ def read_data(regno):
     elif regno == 0:
         return read_dmi("0x4")
 
-def abstract_cmd(addr_size, write, regno):
-    #AARSIZE          22:20
-    #AARPOSTINCREMENT 19
-    #POSTEXEC         18
-    #TRANSFER         17
-    #AARWRITE         16
-    #REGNO            15:0
-    data = 0
-    if write:
-        data = "0x00030000"
-    else:
-        data = "0x00020000"
+
+def access_register(write, register, addr_size):
     addr = "0x17"
+    data = 2**17 # transfer bit always set
+    match addr_size:
+        case 32:
+            data += 2*2**20
+        case 64:
+            data += 3*2**20
+        case 128:
+            data += 4*2**20
+        case _:
+            raise Exception("must provide valid register access size (32, 64, 128). See: 3.7.1.1 aarsize")
+    if write:
+        data += 2**16
+
+    # TODO: convert register alias to regno
+
+    data = hex(data)
     write_dmi(addr, data)
 
 
 def activate_dm():
     write_dmi("0x10", "0x1")
 
+
 def status():
     pass # TODO: check dmstatus and dtmcs err
+
 
 def write_dmi(address, data):
     cmd = f"riscv dmi_write {address} {data}"
     write(cmd)
 
+
 def read_dmi(address):
     cmd = f"riscv dmi_read {address}"
     return execute(cmd)
 
+
 def trst():
     execute("pathmove RESET IDLE")
+
 
 def execute(cmd):
     write(cmd)
     return read()
 
+
 def write(cmd):
     if debug:
         print(f"Executing command: '{cmd}'")
     tn.write(cmd.encode('ascii') + b"\n")
+
 
 def read():
     data = b""
@@ -111,10 +126,8 @@ def read():
             break
         else:
             data += rd
-
     if debug:
         print(data.decode('ascii'))
-
     return data.decode('ascii')
 
 
