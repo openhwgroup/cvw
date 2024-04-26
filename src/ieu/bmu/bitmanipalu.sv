4faf44c4c6a99b4823895d28ec6d6d0e8e4daadf
David Harris <David_Harris@hmc.edu>
2024-04-25 17:24:00 -0700

    Named zknde block in bitmanipalu

--- src/ieu/bmu/bitmanipalu.sv
+++ src/ieu/bmu/bitmanipalu.sv
@@ -112,9 +112,10 @@ module bitmanipalu import cvw::*; #(parameter cvw_t P) (
   end else assign ZBKXResult = '0;
 
   // ZKND and ZKNE AES decryption and encryption
-  if (P.ZKND_SUPPORTED | P.ZKNE_SUPPORTED)
+  if (P.ZKND_SUPPORTED | P.ZKNE_SUPPORTED) begin: zknde
     if (P.XLEN == 32) zknde32 #(P) ZKN32(.A(ABMU), .B(BBMU), .Funct7, .round(Rs2E[3:0]), .ZKNSelect(ZBBSelect[3:0]), .ZKNDEResult); 
     else              zknde64 #(P) ZKN64(.A(ABMU), .B(BBMU), .Funct7, .round(Rs2E[3:0]), .ZKNSelect(ZBBSelect[3:0]), .ZKNDEResult); 
+  end else assign ZKNDEResult = '0;
  
   // ZKNH Unit
   if (P.ZKNH_SUPPORTED) begin: zknh
