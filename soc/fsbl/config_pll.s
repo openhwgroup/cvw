a7051b5f06bc6e45d83367978c65140899b11d7a
Marcus Mellor <infinitymdm@gmail.com>
2024-06-13 09:56:21 -0500

    soc/fsbl/config_pll.s: Config PLL for 2GHz clkout

--- soc/fsbl/config_pll.s
+++ soc/fsbl/config_pll.s
@@ -4,25 +4,26 @@
 
 config_pll:
     # Configure memory-mapped registers for PLL clock generation
+    # This config generates a 2GHz clock at the PLL output, which we divide down
 
     # clkr
     li t1, 0x00020000
-    li t2, 0x1
+    li t2, 0x00
     sd t2, 0(t1)
 
     # clkf
     addi t1, t1, 8
-    li t2, 0x14
+    li t2, 0x0027
     sd t2, 0(t1)
 
     # clkod
     addi t1, t1, 8
-    li t2, 0x2
+    li t2, 0x0
     sd t2, 0(t1)
 
     # bwadj
     addi t1, t1, 8
-    li t2, 0xa
+    li t2, 0x13
     sd t2, 0(t1)
 
     # test
@@ -35,4 +36,11 @@ config_pll:
     li t2, 0x0
     sd t2, 0(t1)
 
+    # wait for lock
+    addi t1, t1, 8
+    ld t2, 0(t1)
+wait_until_pll_locked:
+    bne t2, zero, pll_locked
+    ld t2, 0(t1)
+pll_locked:
     ret
