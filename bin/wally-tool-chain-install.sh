2862b897140b273dc0fd4dca66dada3381478a41
Jordan Carlin <jordanmcarlin@gmail.com>
2024-06-21 21:18:09 -0700

    Update ubuntu toolchain install to match new red hat form in preparation for merging of scripts.

--- bin/wally-tool-chain-install.sh
+++ bin/wally-tool-chain-install.sh
@@ -6,6 +6,7 @@
 ## Created: 18 January 2023
 ## Modified: 22 January 2023
 ## Modified: 23 March 2023
+## Modified: 21 June 2024, Jordan Carlin jcarlin@hmc.edu
 ##
 ## Purpose: Open source tool chain installation script
 ##
@@ -28,8 +29,17 @@
 ## and limitations under the License.
 ################################################################################################
 
-# Use /opt/riscv for installation - may require running script with sudo
-export RISCV="${1:-/opt/riscv}"
+# Check if root
+ROOT=$( [ "${EUID:=$(id -u)}" = 0 ] && echo true || echo false);
+
+# All tools will be installed under the $RISCV directory. By default, if run as root (with sudo)
+# this is set to /opt/riscv. Otherwise, it is set to ~/riscv. This value can be changed if needed.
+if [ "$ROOT" = true ]; then
+  export RISCV="${1:-/opt/riscv}"
+else
+  export RISCV="${1:-$HOME/riscv}"
+fi
+
 export PATH=$PATH:$RISCV/bin:/usr/bin
 
 set -e # break on error
@@ -40,23 +50,39 @@ set -e # break on error
 NUM_THREADS=8  # for >= 32GiB
 #NUM_THREADS=16  # for >= 64GiB
 
-sudo mkdir -p $RISCV
-# *** need to update permissions to local user
+mkdir -p "$RISCV"
 
+echo -e "\n*************************************************************************"
+echo -e "*************************************************************************"
+echo -e "Installing Dependencies from Package Manager"
+echo -e "*************************************************************************"
+echo -e "*************************************************************************\n"
 # Update and Upgrade tools (see https://itsfoss.com/apt-update-vs-upgrade/)
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install -y git gawk make texinfo bison flex build-essential python3 libz-dev libexpat-dev autoconf device-tree-compiler ninja-build libpixman-1-dev ncurses-base ncurses-bin libncurses5-dev dialog curl wget ftp libgmp-dev libglib2.0-dev python3-pip pkg-config opam z3 zlib1g-dev automake autotools-dev libmpc-dev libmpfr-dev  gperf libtool patchutils bc mutt ssmtp
-# Other python libraries used through the book.
-sudo -H pip3 install sphinx sphinx_rtd_theme matplotlib scipy scikit-learn adjustText lief markdown pyyaml 
-sudo -H pip3 install riscv_isac # to generate new tests, such as quads with fp_dataset.py
+sudo apt install -y git gawk make texinfo bison flex build-essential python3 python3-venv libz-dev libexpat-dev autoconf device-tree-compiler ninja-build libpixman-1-dev ncurses-base ncurses-bin libncurses5-dev dialog curl wget ftp libgmp-dev libglib2.0-dev python3-pip pkg-config opam z3 zlib1g-dev automake autotools-dev libmpc-dev libmpfr-dev  gperf libtool patchutils bc mutt ssmtp perl g++ ccache help2man libgoogle-perftools-dev numactl perl-doc
 
-# needed for Ubuntu 22.04, gcc cross compiler expects python not python2 or python3.
-if ! command -v python &> /dev/null
-then
-    echo "WARNING: python3 was installed as python3 rather than python. Creating symlink."
-    sudo ln -sf /usr/bin/python3 /usr/bin/python
+# 24
+# Note, selecting 'zlib1g-dev' instead of 'libz-dev'
+# Note, selecting 'libexpat1-dev' instead of 'libexpat-dev'
+# Note, selecting 'libncurses-dev' instead of 'libncurses5-dev'
+
+
+echo -e "\n*************************************************************************"
+echo -e "*************************************************************************"
+echo -e "Setting up Python Environment"
+echo -e "*************************************************************************"
+echo -e "*************************************************************************\n"
+# Other python libraries used through the book.
+cd "$RISCV"
+if [ ! -e "$RISCV"/riscv-python/bin/activate ]; then
+  python3 -m venv riscv-python
 fi
+source "$RISCV"/riscv-python/bin/activate
+pip install -U pip
+pip install -U sphinx sphinx_rtd_theme matplotlib scipy scikit-learn adjustText lief markdown pyyaml meson testresources riscv_config
+pip install -U riscv_isac # to generate new tests, such as quads with fp_dataset.py
+source "$RISCV"/riscv-python/bin/activate
 
 # gcc cross-compiler (https://github.com/riscv-collab/riscv-gnu-toolchain)
 # To install GCC from source can take hours to compile. 
@@ -65,11 +91,19 @@ fi
 # Versions newer than 2023-12-20 fail to compile the RISC-V arch test with an error:
 # cvw/addins/riscv-arch-test/riscv-test-suite/rv32i_m/I/src/jalr-01.S:72: Error: illegal operands `la x0,5b'
 # PR *** submitted to fix riscv-arch-test to be compatible with latest GCC by modifying test_macros.h for TEST_JALR_OP
-cd $RISCV
-git clone https://github.com/riscv/riscv-gnu-toolchain
-cd riscv-gnu-toolchain
-./configure --prefix=${RISCV} --with-multilib-generator="rv32e-ilp32e--;rv32i-ilp32--;rv32im-ilp32--;rv32iac-ilp32--;rv32imac-ilp32--;rv32imafc-ilp32f--;rv32imafdc-ilp32d--;rv64i-lp64--;rv64ic-lp64--;rv64iac-lp64--;rv64imac-lp64--;rv64imafdc-lp64d--;rv64im-lp64--;"
-make -j 8
+echo -e "\n*************************************************************************"
+echo -e "*************************************************************************"
+echo -e "Installing RISC-V GNU Toolchain"
+echo -e "*************************************************************************"
+echo -e "*************************************************************************\n"
+cd "$RISCV"
+if [[ ((! -e riscv-gnu-toolchain) && ($(git clone https://github.com/riscv/riscv-gnu-toolchain) || true)) || ($(cd riscv-gnu-toolchain; git fetch; git rev-parse HEAD) != $(cd riscv-gnu-toolchain; git rev-parse master)) || (! -e $RISCV/riscv-gnu-toolchain/stamps/build-gcc-newlib-stage2) ]]; then
+  cd riscv-gnu-toolchain
+  git checkout master
+  git pull
+  ./configure --prefix="${RISCV}" --with-multilib-generator="rv32e-ilp32e--;rv32i-ilp32--;rv32im-ilp32--;rv32iac-ilp32--;rv32imac-ilp32--;rv32imafc-ilp32f--;rv32imafdc-ilp32d--;rv64i-lp64--;rv64ic-lp64--;rv64iac-lp64--;rv64imac-lp64--;rv64imafdc-lp64d--;rv64im-lp64--;"
+  make -j ${NUM_THREADS}
+fi
 
 # elf2hex (https://github.com/sifive/elf2hex)
 #The elf2hex utility to converts executable files into hexadecimal files for Verilog simulation. 
@@ -78,51 +112,77 @@ make -j 8
 # For example, if Python version 2.x is in your path, it won’t install correctly. 
 # Also, be sure riscv64-unknown-elf-objcopy shows up in your path in $RISCV/riscv-gnu-toolchain/bin 
 # at the time of compilation, or elf2hex won’t work properly.
-cd $RISCV
+echo -e "\n*************************************************************************"
+echo -e "*************************************************************************"
+echo -e "Installing elf2hex"
+echo -e "*************************************************************************"
+echo -e "*************************************************************************\n"
+cd "$RISCV"
 export PATH=$RISCV/bin:$PATH
-git clone https://github.com/sifive/elf2hex.git
-cd elf2hex
-autoreconf -i
-./configure --target=riscv64-unknown-elf --prefix=$RISCV
-make
-make install
+if [[ ((! -e elf2hex) && ($(git clone https://github.com/sifive/elf2hex.git) || true)) || ($(cd elf2hex; git fetch; git rev-parse HEAD) != $(cd elf2hex; git rev-parse master)) || (! -e $RISCV/bin/riscv64-unknown-elf-elf2bin) ]]; then
+  cd elf2hex
+  git reset --hard && git clean -f && git checkout master && git pull
+  autoreconf -i
+  ./configure --target=riscv64-unknown-elf --prefix="$RISCV"
+  make
+  make install
+fi
 
 
 # QEMU (https://www.qemu.org/docs/master/system/target-riscv.html)
-cd $RISCV
-git clone --recurse-submodules https://github.com/qemu/qemu
-cd qemu
-./configure --target-list=riscv64-softmmu --prefix=$RISCV 
-make -j 8
-make install
+echo -e "\n*************************************************************************"
+echo -e "*************************************************************************"
+echo -e "Installing QEMU"
+echo -e "*************************************************************************"
+echo -e "*************************************************************************\n"
+cd "$RISCV"
+if [[ ((! -e qemu) && ($(git clone --recurse-submodules https://github.com/qemu/qemu) || true)) || ($(cd qemu; git fetch --recurse-submodules=yes; git rev-parse HEAD) != $(cd qemu; git rev-parse master)) || (! -e $RISCV/include/qemu-plugin.h) ]]; then
+  cd qemu
+  git reset --hard && git clean -f && git checkout master && git pull --recurse-submodules
+  ./configure --target-list=riscv64-softmmu --prefix="$RISCV"
+  make -j ${NUM_THREADS}
+  make install
+fi
 
 # Spike (https://github.com/riscv-software-src/riscv-isa-sim)
-# Spike also takes a while to install and compile, but this can be done concurrently 
-# with the GCC installation. 
-cd $RISCV
-git clone https://github.com/riscv-software-src/riscv-isa-sim
-mkdir -p riscv-isa-sim/build
-cd riscv-isa-sim/build
-../configure --prefix=$RISCV 
-make -j 8
-make install 
+# Spike also takes a while to install and compile, but this can be done concurrently
+# with the GCC installation.
+echo -e "\n*************************************************************************"
+echo -e "*************************************************************************"
+echo -e "Installing SPIKE"
+echo -e "*************************************************************************"
+echo -e "*************************************************************************\n"
+cd "$RISCV"
+if [[ ((! -e riscv-isa-sim) && ($(git clone https://github.com/riscv-software-src/riscv-isa-sim) || true)) || ($(cd riscv-isa-sim; git fetch; git rev-parse HEAD) != $(cd riscv-isa-sim; git rev-parse master)) || (! -e $RISCV/lib/pkgconfig/riscv-riscv.pc) ]]; then
+  cd riscv-isa-sim
+  git reset --hard && git clean -f && git checkout master && git pull
+  mkdir -p build
+  cd build
+  ../configure --prefix="$RISCV"
+  make -j ${NUM_THREADS}
+  make install
+fi
 
 
 # Wally needs Verilator 5.021 or later.
 # Verilator needs to be built from scratch to get the latest version
 # apt-get install verilator installs version 4.028 as of 6/8/23
-sudo apt-get install -y perl g++ ccache help2man libgoogle-perftools-dev numactl perl-doc zlib1g 
-cd $RISCV
-git clone https://github.com/verilator/verilator   # Only first time
-# unsetenv VERILATOR_ROOT  # For csh; ignore error if on bash
-unset VERILATOR_ROOT     # For bash
-cd verilator
-git pull         # Make sure git repository is up-to-date
-git checkout master      
-autoconf         # Create ./configure script
-./configure      # Configure and create Makefile
-make -j 8  # Build Verilator itself (if error, try just 'make')
-sudo make install
+echo -e "\n*************************************************************************"
+echo -e "*************************************************************************"
+echo -e "Installing Verilator"
+echo -e "*************************************************************************"
+echo -e "*************************************************************************\n"
+cd "$RISCV"
+if [[ ((! -e verilator) && ($(git clone https://github.com/verilator/verilator) || true)) || ($(cd verilator; git fetch; git rev-parse HEAD) != $(cd verilator; git rev-parse master)) || (! -e $RISCV/share/pkgconfig/verilator.pc) ]]; then
+  # unsetenv VERILATOR_ROOT  # For csh; ignore error if on bash
+  unset VERILATOR_ROOT     # For bash
+  cd verilator
+  git reset --hard && git clean -f && git checkout master && git pull
+  autoconf         # Create ./configure script
+  ./configure --prefix="$RISCV"     # Configure and create Makefile
+  make -j ${NUM_THREADS}  # Build Verilator itself (if error, try just 'make')
+  make install
+fi
 
 # Sail (https://github.com/riscv/sail-riscv)
 # Sail is the new golden reference model for RISC-V.  Sail is written in OCaml, which 
@@ -130,63 +190,47 @@ sudo make install
 # language suited to formal verification.  OCaml is installed with the opam OCcaml 
 # package manager. Sail has so many dependencies that it can be difficult to install.
 # This script works for Ubuntu.
-
-# Alex Solomatnikov found these commands worked to build Sail for Centos 8 on 1/12/24
-#sudo su -
-#dnf install ocaml.x86_64
-#pip3 install z3-solver
-#wget https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh
-#sh install.sh
-#opam init
-#exit
-#ocaml -version
-#opam switch create 5.1.0
-#eval $(opam config env)
-#git clone --recurse-submodules git@github.com:riscv/sail-riscv.git
-#cd sail-riscv
-#make
-#ARCH=RV32 make
-#ARCH=RV64 make
-#git log -1
-#cp -p c_emulator/riscv_sim_RV* /tools/sail-riscv/d7a3d8012fd579f40e53a29569141d72dd5e0c32/bin/.
-
-
-# This was an earlier attemp to prepare to install Sail on RedHat 8
-# Do these commands only for RedHat / Rocky 8 to build from source.
-#cd $RISCV
-#git clone https://github.com/Z3Prover/z3.git
-#cd z3
-#python scripts/mk_make.py
-#cd build
-#make  -j 8
-#make install
-#cd ../..
-#pip3 install chardet==3.0.4
-#pip3 install urllib3==1.22
-
-cd $RISCV
+echo -e "\n*************************************************************************"
+echo -e "*************************************************************************"
+echo -e "Installing Sail Compiler"
+echo -e "*************************************************************************"
+echo -e "*************************************************************************\n"
+cd "$RISCV"
 opam init -y --disable-sandboxing
-opam update
-opam upgrade
-opam switch create 5.1.0
-opam install sail -y 
+opam update -y
+opam upgrade -y
+opam switch create 5.1.0 || opam switch set 5.1.0
+opam install sail -y
 
 eval $(opam config env)
-git clone https://github.com/riscv/sail-riscv.git
-cd sail-riscv
-# For now, use checkout that is stable for Wally
-#git checkout 72b2516d10d472ac77482fd959a9401ce3487f60  # not new enough for Zicboz?
-export OPAMCLI=2.0  # Sail is not compatible with opam 2.1 as of 4/16/24
-ARCH=RV64 make -j 8 c_emulator/riscv_sim_RV64
-ARCH=RV32 make -j 8 c_emulator/riscv_sim_RV32
-sudo ln -sf $RISCV/sail-riscv/c_emulator/riscv_sim_RV64 /usr/bin/riscv_sim_RV64
-sudo ln -sf $RISCV/sail-riscv/c_emulator/riscv_sim_RV32 /usr/bin/riscv_sim_RV32
+if [[ ((! -e sail-riscv) && ($(git clone https://github.com/riscv/sail-riscv.git) || true)) || ($(cd sail-riscv; git fetch; git rev-parse HEAD) != $(cd sail-riscv; git rev-parse master)) || (! -e $RISCV/bin/riscv_sim_RV32) ]]; then
+  cd sail-riscv
+  git reset --hard && git clean -f && git checkout master && git pull
+  export OPAMCLI=2.0  # Sail is not compatible with opam 2.1 as of 4/16/24
+  ARCH=RV64 make -j ${NUM_THREADS} c_emulator/riscv_sim_RV64
+  ARCH=RV32 make -j ${NUM_THREADS} c_emulator/riscv_sim_RV32
+  cd "$RISCV"
+  ln -sf ../sail-riscv/c_emulator/riscv_sim_RV64 bin/riscv_sim_RV64
+  ln -sf ../sail-riscv/c_emulator/riscv_sim_RV32 bin/riscv_sim_RV32
+fi
 
 # riscof
-sudo -H pip3 install -U testresources riscv_config
-sudo -H pip3 install git+https://github.com/riscv/riscof.git
+echo -e "\n*************************************************************************"
+echo -e "*************************************************************************"
+echo -e "Installing riscof"
+echo -e "*************************************************************************"
+echo -e "*************************************************************************\n"
+pip3 install git+https://github.com/riscv/riscof.git
 
 # Download OSU Skywater 130 cell library
-sudo mkdir -p $RISCV/cad/lib
-cd $RISCV/cad/lib
-sudo git clone https://foss-eda-tools.googlesource.com/skywater-pdk/libs/sky130_osu_sc_t12
+echo -e "\n*************************************************************************"
+echo -e "*************************************************************************"
+echo -e "Installing OSU Skywater 130 cell library"
+echo -e "*************************************************************************"
+echo -e "*************************************************************************\n"
+mkdir -p "$RISCV"/cad/lib
+cd "$RISCV"/cad/lib
+if [[ ((! -e sky130_osu_sc_t12) && ($(git clone https://foss-eda-tools.googlesource.com/skywater-pdk/libs/sky130_osu_sc_t12) || true)) || ($(cd sky130_osu_sc_t12; git fetch; git rev-parse HEAD) != $(cd sky130_osu_sc_t12; git rev-parse master)) ]]; then
+  cd sky130_osu_sc_t12
+  git reset --hard && git clean -f && git checkout master && git pull
+fi
