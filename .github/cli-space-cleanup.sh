cd0c090658387552d376d7d10ca040f440cd77f5
Jordan Carlin <jordanmcarlin@gmail.com>
2024-07-17 00:59:51 -0700

    Update cli-space-cleanup.sh

--- .github/cli-space-cleanup.sh
+++ .github/cli-space-cleanup.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 removePacks=( '^llvm-.*' 'php.*' '^mongodb-.*' '^mysql-.*' '^dotnet-sdk-.*' 'azure-cli' 'google-cloud-cli' 'google-chrome-stable' 'firefox' 'powershell*' 'microsoft-edge-stable' 'mono-devel' )
 for pack in "${removePacks[@]}"; do
   echo "REMOVING ${pack}"
