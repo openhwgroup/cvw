de3bac0f59d7c81d80d373d2525780a1cea5f735
Jordan Carlin <jordanmcarlin@gmail.com>
2024-07-17 01:50:19 -0700

    Update cli-space-saver

--- .github/cli-space-cleanup.sh
+++ .github/cli-space-cleanup.sh
@@ -30,37 +30,37 @@
 # Remove unnecessary packages
 removePacks=( '^llvm-.*' 'php.*' '^mongodb-.*' '^mysql-.*' '^dotnet-sdk-.*' 'azure-cli' 'google-cloud-cli' 'google-chrome-stable' 'firefox' '^powershell*' 'microsoft-edge-stable' 'mono-devel' 'hhvm' )
 for pack in "${removePacks[@]}"; do
-  sudo apt-get purge -y $pack &> /dev/null || true
+  sudo apt-get purge -y $pack || true
 done
-sudo apt-get autoremove -y &> /dev/null || true
-sudo apt-get clean &> /dev/null || true
+sudo apt-get autoremove -y || true
+sudo apt-get clean || true
 
 # Remove unnecessary directories
-sudo rm -rf /usr/local/lib/android &> /dev/null
-sudo rm -rf /usr/share/dotnet &> /dev/null
-sudo rm -rf /usr/share/swift &> /dev/null
-sudo rm -rf /usr/share/miniconda &> /dev/null
-sudo rm -rf /usr/share/az* &> /dev/null
-sudo rm -rf /usr/share/gradle-* &> /dev/null
-sudo rm -rf /usr/share/sbt &> /dev/null
-sudo rm -rf /opt/ghc &> /dev/null
-sudo rm -rf /usr/local/.ghcup &> /dev/null
-sudo rm -rf /usr/local/share/powershell &> /dev/null
-sudo rm -rf /usr/local/lib/node_modules &> /dev/null
-sudo rm -rf /usr/local/julia* &> /dev/null
-sudo rm -rf /usr/local/share/chromium &> /dev/null
-sudo rm -rf /usr/local/share/vcpkg &> /dev/null
-sudo rm -rf /usr/local/games &> /dev/null
-sudo rm -rf /usr/local/sqlpackage &> /dev/null
-sudo rm -rf /usr/lib/google-cloud-sdk &> /dev/null
-sudo rm -rf /usr/lib/jvm &> /dev/null
-sudo rm -rf /usr/lib/mono &> /dev/null
-sudo rm -rf /usr/lib/R &> /dev/null
-sudo rm -rf /usr/lib/postgresql &> /dev/null
-sudo rm -rf /usr/lib/heroku &> /dev/null
-sudo rm -rf /usr/lib/llvm* &> /dev/null
-sudo rm -rf /usr/lib/firefox &> /dev/null
-sudo rm -rf /opt/hostedtoolcache &> /dev/null
+sudo rm -rf /usr/local/lib/android
+sudo rm -rf /usr/share/dotnet
+sudo rm -rf /usr/share/swift
+sudo rm -rf /usr/share/miniconda
+sudo rm -rf /usr/share/az*
+sudo rm -rf /usr/share/gradle-*
+sudo rm -rf /usr/share/sbt
+sudo rm -rf /opt/ghc
+sudo rm -rf /usr/local/.ghcup
+sudo rm -rf /usr/local/share/powershell
+sudo rm -rf /usr/local/lib/node_modules
+sudo rm -rf /usr/local/julia*
+sudo rm -rf /usr/local/share/chromium
+sudo rm -rf /usr/local/share/vcpkg
+sudo rm -rf /usr/local/games
+sudo rm -rf /usr/local/sqlpackage
+sudo rm -rf /usr/lib/google-cloud-sdk
+sudo rm -rf /usr/lib/jvm
+sudo rm -rf /usr/lib/mono
+sudo rm -rf /usr/lib/R
+sudo rm -rf /usr/lib/postgresql
+sudo rm -rf /usr/lib/heroku
+sudo rm -rf /usr/lib/llvm*
+sudo rm -rf /usr/lib/firefox
+sudo rm -rf /opt/hostedtoolcache
 
 # Clean up docker images
-sudo docker image prune --all --force &> /dev/null
+sudo docker image prune --all --force
