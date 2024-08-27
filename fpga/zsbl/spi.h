44ece7cb968655401e0c8f2b45ebf4837310bd33
Jacob Pease <jacobpease@protonmail.com>
2024-08-27 14:28:49 -0500

    Added CVW header to spitest files.

--- fpga/zsbl/spi.h
+++ fpga/zsbl/spi.h
@@ -1,3 +1,32 @@
+///////////////////////////////////////////////////////////////////////
+// spi.h
+//
+// Written: Jaocb Pease jacob.pease@okstate.edu 7/22/2024
+//
+// Purpose: Header file for interfaceing with the SPI peripheral
+//
+// 
+//
+// A component of the Wally configurable RISC-V project.
+// 
+// Copyright (C) 2021-23 Harvey Mudd College & Oklahoma State University
+//
+// SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+//
+// Licensed under the Solderpad Hardware License v 2.1 (the
+// “License”); you may not use this file except in compliance with the
+// License, or, at your option, the Apache License version 2.0. You
+// may obtain a copy of the License at
+//
+// https://solderpad.org/licenses/SHL-2.1/
+//
+// Unless required by applicable law or agreed to in writing, any work
+// distributed under the License is distributed on an “AS IS” BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied. See the License for the specific language governing
+// permissions and limitations under the License.
+///////////////////////////////////////////////////////////////////////
+
 #pragma once
 #ifndef SPI_HEADER
 #define SPI_HEADER
