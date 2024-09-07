de26b7b6a792f1993a519fa76de187630d8d13af
David Harris <David_Harris@hmc.edu>
2024-09-07 07:00:52 -0700

    Per config coverage initially working with RV32M in rv32gc config

--- config/rv32gc/coverage_extensions.svh
+++ config/rv32gc/coverage_extensions.svh
@@ -5,5 +5,7 @@
 // This file is needed in the config subdirectory for each config supporting coverage.
 // It defines which extensions are enabled for that config.
 
-`define COVER_BASE_RV32I
+`define COVER_RV32I
+`define COVER_RV32M
 `include "coverage/RV32I_coverage.svh"
+`include "coverage/RV32M_coverage.svh"
