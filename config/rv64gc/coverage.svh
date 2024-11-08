18ddf1f77b7bafe8831532efa833d392d46d9b32
David Harris <74973295+davidharrishmc@users.noreply.github.com>
2024-11-07 17:04:09 -0800

    Revert "Enabling ZB and Zk extensions in coverage.svh of congif/rv32gc and congif/rv64gc"

--- config/rv64gc/coverage.svh
+++ config/rv64gc/coverage.svh
@@ -10,13 +10,6 @@
 `include "RV64M_coverage.svh"
 `include "RV64F_coverage.svh"
 `include "RV64D_coverage.svh"
-`include "RV64Zba_coverage.svh"
-`include "RV64Zbb_coverage.svh"
-`include "RV64Zbc_coverage.svh"
-`include "RV64Zbs_coverage.svh"
-`include "RV64Zbkb_coverage.svh"
-`include "RV64Zbkc_coverage.svh"
-`include "RV64Zbkx_coverage.svh"
 `include "RV64ZfaF_coverage.svh"
 `include "RV32ZfaD_coverage.svh"
 `include "RV32ZfaZfh_coverage.svh"
@@ -29,18 +22,6 @@
 `include "RV64ZcbZbb_coverage.svh"
 `include "RV64ZcbZba_coverage.svh"
 `include "RV64Zcd_coverage.svh"
-`include "RV64Zaamo_coverage.svh"
-`include "RV64Zalrsc_coverage.svh"
-`include "RV64Zknd_coverage.svh"
-`include "RV64Zkne_coverage.svh"
-`include "RV64Zknh_coverage.svh"
-
-
-
-
-
-
-
 
 // Privileged extensions
 `include "RV64VM_coverage.svh"
