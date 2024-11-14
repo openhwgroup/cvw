d41dc8d2def2090b9e6494dad9f1d174f704d319
Ahlyssa Santillana <asantillana@hmc.edu>
2024-11-14 06:26:28 -0800

    incorportated Zicsr to run in Imperas

--- config/rv64gc/coverage.svh
+++ config/rv64gc/coverage.svh
@@ -26,6 +26,7 @@
 `include "RV64ZcbZbb_coverage.svh"
 `include "RV64ZcbZba_coverage.svh"
 `include "RV64Zcd_coverage.svh"
+`include "RV64Zicsr_coverage.svh"
 
 // Privileged extensions
 `include "RV64VM_coverage.svh"
