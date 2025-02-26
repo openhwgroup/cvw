db9689e60f824b1908458cc8511d7e51bc1d9098
David Harris <David_Harris@hmc.edu>
2025-02-26 11:42:36 -0800

    Completed script for Lab 4

--- examples/exercises/16p1/antimul.sh
+++ examples/exercises/16p1/antimul.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-#sed -z 's/assign Aprime .*Memory stage//' $WALLY/src/mdu/mul.sv
+sed -i 's/assign MFunctD.*/assign MFunctD = 0; \/\/ *** Replace with your logic/' $WALLY/src/ieu/controller.sv
+sed -i 's/ControlsD.*Divide/assign ControlsD = 0; \/\/ *** Replace with your logic/' $WALLY/src/ieu/controller.sv
 sed -zi 's/\/\/ Number systems.*logic/  logic/' $WALLY/src/mdu/mul.sv
 sed -zi 's/assign Aprime.*Memory/\/\/ Memory/' $WALLY/src/mdu/mul.sv
