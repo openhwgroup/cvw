91e2c150e2e35bfaec1e5a268144a49db6a138d4
James Stine <james.stine@okstate.edu>
2025-04-08 17:06:55 -0500

    add SPDX header + change order for lint

--- bin/analyze_riscv_elf.py
+++ bin/analyze_riscv_elf.py
@@ -1,19 +1,40 @@
 #!/usr/bin/env python3
 
-"""
-james.stine@okstate.edu 8 April 2025
+###########################################
+## analyze_riscv_elf.py
+##
+## Written: james.stine@okstate.edu
+## Created: April 7, 2025
+##
+## Purpose: Analyze a RISC-V ELF file and report which instructions are used,
+##          along with their frequency. The script disassembles the ELF using
+##          riscv64-unknown-elf-objdump, filters real instructions (excluding
+##          pseudo-ops and section headers), and displays a histogram of 
+##          instruction use.Simulate a L1 D$ or I$ for comparison with Wally
+##
+## A component of the CORE-V-WALLY configurable RISC-V project.
+## https://github.com/openhwgroup/cvw
+##
+## Copyright (C) 2021-25 Harvey Mudd College & Oklahoma State University
+##
+## SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+##
+## Licensed under the Solderpad Hardware License v 2.1 (the “License”); you may not use this file
+## except in compliance with the License, or, at your option, the Apache License version 2.0. You
+## may obtain a copy of the License at
+##
+## https:##solderpad.org/licenses/SHL-2.1/
+##
+## Unless required by applicable law or agreed to in writing, any work distributed under the
+## License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+## either express or implied. See the License for the specific language governing permissions
+## and limitations under the License.
+################################################################################################
 
-Purpose: Analyze a RISC-V ELF file and report which instructions are used,
-         along with their frequency. The script disassembles the ELF using
-         riscv64-unknown-elf-objdump, filters real instructions (excluding
-         pseudo-ops and section headers), and displays a histogram of 
-         instruction use.
-"""
-
+from collections import Counter
 import subprocess
 import sys
 import re
-from collections import Counter
 
 def disassemble_elf(elf_path):
     try:
