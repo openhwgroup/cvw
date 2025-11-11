336e2da7815cc404e57be0f3caeb493a9aeeeccd
James Stine <james.stine@okstate.edu>
2025-11-11 07:47:55 -0600

    update simpler solution on check_tool to avoid outdated SKY130 install

--- bin/installation/skywater-lib-install.sh
+++ bin/installation/skywater-lib-install.sh
@@ -31,11 +31,7 @@ SKYWATER_LIB_VERSION=3e7dac7af98731b59982f99df6a71e979a44bff7 # Last commit as o
 
 # Ensure repo always comes from NEW GitHub
 if [ -d "$RISCV/cad/lib/sky130_osu_sc_t12/.git" ]; then
-  git -C "$RISCV/cad/lib/sky130_osu_sc_t12" remote set-url origin https://github.com/stineje/sky130_osu_sc_t12.git
-  git -C "$RISCV/cad/lib/sky130_osu_sc_t12" fetch --all --prune
-  git -C "$RISCV/cad/lib/sky130_osu_sc_t12" reset --hard "$SKYWATER_LIB_VERSION"
-else
-  rm -rf "$RISCV/cad/lib/sky130_osu_sc_t12"
+  rm -rf $RISCV/cad/lib/sky130_osu_sc_t12/.git
 fi
 
 set -e # break on error
