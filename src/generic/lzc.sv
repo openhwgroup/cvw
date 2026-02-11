62c60a15594ff3e46759da1a22f39b05b0b2fb53
James Stine <james.stine@okstate.edu>
2026-02-11 09:41:20 -0600

    Issue 1469: Replace bitwise & with logical && and ~ with ! in the while condition. Ensures short-circuit evaluation, prevents potential out-of-range indexing, and aligns with Quartus synthesis expectations for Boolean control logic. Evaluated validation and synthesis for sky90 and tsmc28 that work satisfactorily. Believe that this modification satisifes the issue noted.

--- src/generic/lzc.sv
+++ src/generic/lzc.sv
@@ -2,6 +2,7 @@
 //
 // Written: me@KatherineParry.com
 // Modified: 7/5/2022
+// Modified: 2/11/2026 james.stine@okstate.edu/marcus@infinitymdm.dev 
 //
 // Purpose: Leading Zero Counter
 //
@@ -24,7 +25,7 @@
 // and limitations under the License.
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
-module lzc #(parameter WIDTH = 1) (
+module lzc #(parameter int WIDTH = 1) (
   input  logic [WIDTH-1:0]            num,    // number to count the leading zeroes of
   output logic [$clog2(WIDTH+1)-1:0]  ZeroCnt // the number of leading zeroes
 );
@@ -33,7 +34,10 @@ module lzc #(parameter WIDTH = 1) (
 
   always_comb begin
     i = 0;
-    while ((i < WIDTH) & ~num[WIDTH-1-i]) i = i+1;  // search for leading one
+    // search for leading one
+    while ((i < WIDTH) && (!num[WIDTH-1-i])) begin
+      i = i + 1;
+    end
     ZeroCnt = i[$clog2(WIDTH+1)-1:0];
   end
 endmodule
